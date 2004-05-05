899d039fd3131a0cc948d47c9194ad5edf14897a
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2004-05-05 17:40:04 +0000

    added $Id$

--- w3m-init.el
+++ w3m-init.el
@@ -1,4 +1,6 @@
-;$Id$ -*- emcs-lisp -*-
+; -*- emcs-lisp -*-
+; $Id: tiarra-conf.el,v 1.2 2003/05/21 04:17:25 admin Exp $
+
 (setq w3m-tab-width 4)
 (setq w3m-use-cookies t)
 (setq w3m-use-toolbar t)
