b7c7987e6da18172d6030c0e41cd593f70cfb512
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2004-05-10 09:05:13 +0000

    added (load wget.el )

--- w3m-init.el
+++ w3m-init.el
@@ -1,8 +1,10 @@
 ; -*- emcs-lisp -*-
-; $Id: tiarra-conf.el,v 1.2 2003/05/21 04:17:25 admin Exp $
+; $Id$
 
 (setq w3m-tab-width 4)
 (setq w3m-use-cookies t)
 (setq w3m-use-toolbar t)
 (setq w3m-home-page "~/.w3m/bookmark.html")
 (setq w3m-weather-default-area "道央・石狩")
+
+(load "w3m-wget")
