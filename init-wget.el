176dc87bea2f08020668eccf875c66adc474865c
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2004-05-10 10:13:41 +0000

    added init-wget.el

---
+++ init-wget.el
@@ -0,0 +1,5 @@
+; -*- emcs-lisp -*-
+; $Id: init-w3m.el 50 2004-05-10 09:07:55Z takeru $
+
+(autoload 'wget "wget" "wget interface for Emacs." t)
+(autoload 'wget-web-page "wget" "wget interface to download whole web page." t)
