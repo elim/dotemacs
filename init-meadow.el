0baa55a8952b9808db369d6204d836786d45ebc3
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2005-05-17 07:26:52 +0000

    add X Window System Setting.

--- init-meadow.el
+++ init-meadow.el
@@ -1,44 +1,36 @@
-;-*- emcs-lisp -*-
+;-*- emacs-lisp -*-
 ; $Id$
 
-(w32-add-font
- "BDF M+"
- '((strict-spec
-    ((:char-spec ascii :height any)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_f12r.bdf"))
-    ((:char-spec ascii :height any :weight bold)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_f12b.bdf"))
-    ((:char-spec ascii :height any :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_f12i.bdf"))
-    ((:char-spec ascii :height any :weight  bold :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_f12bi.bdf"))
-    ((:char-spec japanese-jisx0208 :height any)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_j12r.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :weight bold)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_j12b.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_j12i.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :weight bold :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/mplus/mplus_j12bi.bdf")))))
+;; XWindow System
+(eval-safe
+ (progn
+   (set-face-font 'default
+		  "-shinonome-gothic-medium-r-normal--14-0-0-0-c-0-*-*")
+   (set-face-font 'bold
+		  "-shinonome-gothic-bold-r-normal--14-0-0-0-c-0-*-*")
+   (set-face-font 'bold-italic
+		  "-shinonome-gothic-bold-i-normal--14-0-0-0-c-0-*-*")))
 
-(w32-add-font
- "shinonome 14"
- '((strict-spec
-    ((:char-spec ascii :height any)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14r.bdf"))
-    ((:char-spec ascii :height any :weight bold)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14rb.bdf"))
-    ((:char-spec ascii :height any :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14ri.bdf"))
-    ((:char-spec ascii :height any :weight  bold :slant italic)
+;; Meadow 2.x
+(eval-safe
+ (w32-add-font
+  "shinonome 14"
+  '((strict-spec
+     ((:char-spec ascii :height any)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14r.bdf"))
+     ((:char-spec ascii :height any :weight bold)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14rb.bdf"))
+     ((:char-spec ascii :height any :slant italic)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14ri.bdf"))
+     ((:char-spec ascii :height any :weight  bold :slant italic)
      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnm7x14rbi.bdf"))
-    ((:char-spec japanese-jisx0208 :height any)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :weight bold)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14b.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14i.bdf"))
-    ((:char-spec japanese-jisx0208 :height any :weight bold :slant italic)
-     (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14bi.bdf")))))
+     ((:char-spec japanese-jisx0208 :height any)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14.bdf"))
+     ((:char-spec japanese-jisx0208 :height any :weight bold)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14b.bdf"))
+     ((:char-spec japanese-jisx0208 :height any :slant italic)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14i.bdf"))
+     ((:char-spec japanese-jisx0208 :height any :weight bold :slant italic)
+      (bdf-font "c:/cygwin/home/takeru/lib/X11/fonts/shinonome/shnmk14bi.bdf")))))
 
-(set-default-font "shinonome 14")
+ (set-default-font "shinonome 14"))
