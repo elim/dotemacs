b865fe555ba8e6625d65be65b67d9245fe6fe2ce
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2005-06-30 11:16:47 +0000

    change uri.

--- init-tdiary-mode.el
+++ init-tdiary-mode.el
@@ -6,20 +6,10 @@
 (autoload 'tdiary-update "tdiary-mode" nil t)
 (autoload 'tdiary-replace "tdiary-mode" nil t)
 
-
 (setq tdiary-passwd-file (expand-file-name "~/.tdiary-password"))
 (setq tdiary-diary-list
-      '(("p"
-	 "http://elim.teroknor.org/%7Etakeru/private_log/")
-	("pl"
-	 "http://localhost/%7Etakeru/private_log/")
-	("l"
-	 "http://elim.teroknor.org/%7Etakeru/life_log/")
-	("ll"
-	 "http://localhost/%7Etakeru/life_log/")
-	("x"
-	 "http://elim.s27.xrea.com/LifeLog/" "index.cgi" "update.cgi")))
+      '(("l"
+	 "http://elim.teroknor.org/%7Etakeru/life_log/")))
 
 (setq psgml-html-build-new-buffer nil)
-
 (setq tdiary-browser-function 'browse-url)
