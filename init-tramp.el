fc8682ee3cf0253ac1c0b6b20644e053fffd8f43
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2006-01-07 01:59:16 +0000

    * init-tramp.el: - site-start で tramp が require されない環境もあるので, 明示的に require するようにした.

--- init-tramp.el
+++ init-tramp.el
@@ -2,7 +2,7 @@
 ;; http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=175346
 ;; http://yoichi.geiin.org/d/?date=20030328#p01
 
-(when (locate-library "tramp")
+(when (require 'tramp nil t)
   (setq auto-save-file-name-transforms
 	`(("\\`/[^/]*:\\(.+/\\)*\\(.*\\)"
 	   ,(expand-file-name "\\2" temporary-file-directory)))))
