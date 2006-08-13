4a170d39f98781cb94ebc94d2a4c6074428d69d2
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2006-08-13 06:37:39 +0000

    * init-frame.el: - window-system が non-nil の時だけ大きさの保存・復帰を行うようにした.

--- init-frame.el
+++ init-frame.el
@@ -20,43 +20,44 @@
 
 ;; http://www.bookshelf.jp/cgi-bin/goto.cgi?file=meadow&node=save%20framesize
 (defun window-size-save ()
-  (let* ((rlist (frame-parameters (selected-frame)))
-         (ilist initial-frame-alist)
-         (nCHeight (frame-height))
-         (nCWidth (frame-width))
-         (tMargin (if (integerp (cdr (assoc 'top rlist)))
-                      (cdr (assoc 'top rlist)) 0))
-         (lMargin (if (integerp (cdr (assoc 'left rlist)))
-                      (cdr (assoc 'left rlist)) 0))
-         buf
-         (file frame-size-configuration-file))
-    (if (get-file-buffer (expand-file-name file))
-        (setq buf (get-file-buffer (expand-file-name file)))
-      (setq buf (find-file-noselect file)))
-    (set-buffer buf)
-    (erase-buffer)
-    (insert (concat
-             ;; 初期値をいじるよりも modify-frame-parameters
-             ;; で変えるだけの方がいい?
-             "(delete 'width initial-frame-alist) "
-             "(delete 'height initial-frame-alist) "
-             "(delete 'top initial-frame-alist) "
-             "(delete 'left initial-frame-alist) "
-             "(setq initial-frame-alist "
-	     "(append (list "
-             "'(width . " (int-to-string nCWidth) ") "
-             "'(height . " (int-to-string nCHeight) ") "
-             "'(top . " (int-to-string tMargin) ") "
-             "'(left . " (int-to-string lMargin) ")) "
-             "initial-frame-alist)) "
-             "(setq default-frame-alist initial-frame-alist)"
-             ))
-    (save-buffer)))
+  (when window-system
+    (let* ((rlist (frame-parameters (selected-frame)))
+	   (ilist initial-frame-alist)
+	   (nCHeight (frame-height))
+	   (nCWidth (frame-width))
+	   (tMargin (if (integerp (cdr (assoc 'top rlist)))
+			(cdr (assoc 'top rlist)) 0))
+	   (lMargin (if (integerp (cdr (assoc 'left rlist)))
+			(cdr (assoc 'left rlist)) 0))
+	   buf
+	   (file frame-size-configuration-file))
+      (if (get-file-buffer (expand-file-name file))
+	  (setq buf (get-file-buffer (expand-file-name file)))
+	(setq buf (find-file-noselect file)))
+      (set-buffer buf)
+      (erase-buffer)
+      (insert (concat
+	       ;; 初期値をいじるよりも modify-frame-parameters
+	       ;; で変えるだけの方がいい?
+	       "(delete 'width initial-frame-alist) "
+	       "(delete 'height initial-frame-alist) "
+	       "(delete 'top initial-frame-alist) "
+	       "(delete 'left initial-frame-alist) "
+	       "(setq initial-frame-alist "
+	       "(append (list "
+	       "'(width . " (int-to-string nCWidth) ") "
+	       "'(height . " (int-to-string nCHeight) ") "
+	       "'(top . " (int-to-string tMargin) ") "
+	       "'(left . " (int-to-string lMargin) ")) "
+	       "initial-frame-alist)) "
+	       "(setq default-frame-alist initial-frame-alist)" ))
+      (save-buffer))))
 
 (defun window-size-load ()
-  (let* ((file frame-size-configuration-file))
-    (if (file-exists-p file)
-        (load file))))
+  (when window-system
+    (let* ((file frame-size-configuration-file))
+      (if (file-exists-p file)
+	  (load file)))))
 
 (window-size-load)
 
@@ -64,4 +65,3 @@
 (defadvice save-buffers-kill-emacs
   (before save-frame-size activate)
   (window-size-save))
-
