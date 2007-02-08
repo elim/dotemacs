c281bd91b628e831bf218c604637670ff05d475b
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-02-08 18:15:41 +0000

    update music folder.

--- init-mpg123.el
+++ init-mpg123.el
@@ -6,4 +6,4 @@
   (setq mpg123-startup-volume 70)       ; 起動時の音量
   (setq mpg123-default-repeat -1)       ; 繰り返し回数。-1は永遠に繰り返す。
   (setq mpg123-default-dir              ; 起動時のディレクトリ
-	(expand-file-name "/opt/share/iTunes/iTunes Music/")))
+	(expand-file-name "/opt/share/music/")))
