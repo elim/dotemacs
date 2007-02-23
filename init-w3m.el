350d5e8c5c41988cab6eb1f3437becbc4392bffb
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-02-23 11:20:13 +0000

    add function `w3m-filter-amazon'

--- init-w3m.el
+++ init-w3m.el
@@ -55,3 +55,41 @@
 	    (body-presentation-method . mime-w3m-preview-text/html)))
 	 (set-alist 'mime-view-type-subtype-score-alist
 		    '(text . html) 3)))))
+
+
+;; http://ko.meadowy.net/%7Eshirai/diary/20070223.html#p01
+(setq w3m-use-filter t)
+(setq w3m-filter-rules
+      `(("\\`https?://\\(www.\\)?amazon\\.co\\.jp/.+" w3m-filter-amazon)))
+
+(defun w3m-filter-amazon (url)
+  (when (and url
+	     (or (string-match
+		  "^\\(https?://\\(?:www\\.\\)?amazon\\.co\\.jp\\)/exec/obidos/ASIN/\\([0-9]+\\)"
+		  url)
+		 (string-match
+		  "^\\(https?://\\(?:www\\.\\)?amazon\\.co\\.jp\\)/o/ASIN/\\([0-9]+\\)"
+		  url)
+		 (string-match
+		  "^\\(https?://\\(?:www\\.\\)?amazon\\.co\\.jp\\)/gp/product/\\([0-9]+\\)"
+		  url)
+		 (string-match
+		  "^\\(https?://\\(?:www\\.\\)?amazon\\.co\\.jp\\)/\\(?:[^/]+/\\)?dp/\\([0-9]+\\)"
+		  url)))
+    (let* ((base (match-string 1 url))
+	   (asin (match-string 2 url))
+	   (shorturls `(,(concat base "/dp/" asin "/")
+			,(concat base "/o/ASIN/" asin "/")
+			,(concat base "/gp/product/" asin "/")))
+	   (case-fold-search t)
+	   shorturl)
+      (goto-char (point-min))
+      (setq url (file-name-as-directory url))
+      (when (search-forward "<body>" nil t)
+	(insert "\n")
+	(while (setq shorturl (car shorturls))
+	  (setq shorturls (cdr shorturls))
+	  (unless (string= url shorturl)
+	    (insert (format "Amazon Short URL: <a href=\"%s\">%s</a><br>\n"
+			    shorturl shorturl))))
+	(insert "\n")))))
