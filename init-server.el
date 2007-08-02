96acf3411040bdc542404bc1269b1d24ec9418a1
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-08-02 13:35:47 +0000

    enabled on meadow.

--- init-server.el
+++ init-server.el
@@ -1,18 +1,6 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
-(when (and (functionp 'server-start)
-	   (not (featurep 'meadow)))
+(when (functionp 'server-start)
   (setq server-window 'pop-to-buffer)
-
-  ;; svn.*tmp を開いたら Changelog mode にする.
-  ;; やめた. 
-  ;;   (when (functionp 'add-change-log-entry)
-  ;;     (add-hook 'server-visit-hook
-  ;; 	      (lambda ()
-  ;; 		(when (string-match "svn.*tmp" buffer-file-name)
-  ;; 		  (add-change-log-entry nil (buffer-file-name))))))
-  
   (server-start))
-
-
