d03fe65a87449d0cc3332993f2d11caf3a23e2b0
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-09-26 20:33:47 +0000

    added function get-ip-address. changed function domestic-member-p use get-ip-address. changed loading method for initialization files.

--- init.el
+++ init.el
@@ -1,40 +1,29 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
-;; functions
+;;; path and filenames.
+(setq my-lisp-path (expand-file-name "~/.emacs.d/"))
+(setq custom-file
+      (expand-file-name (concat my-lisp-path "/customize.el")))
+
 (defun my-add-path (target-list path-list)
   (condition-case err
-	(eval target-list)
+       (eval target-list)
     (error (set target-list (list))))
   (dolist (p (reverse path-list))
     (when (file-accessible-directory-p p)
       (add-to-list target-list
 		   (expand-file-name p)))))
 
-(defun domestic-network-member-p ()
-  (let
-      ((domestic-domain
-	"fascinating.local$")
-       (airport
-	"/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport")
-       (fon-bssid
-	"00:18:84:1a:df:d6"))
-    (or
-     (string-match domestic-domain system-name)
-     (and
-      (string= system-type "darwin")
-      (string-match fon-bssid
-		    (shell-command-to-string
-		     (concat airport " --getinfo | grep BSSID")))))))
-
-;; variables
-(setq my-lisp-path (expand-file-name "~/.emacs.d/"))
-
 (my-add-path 'load-path
 	     (list my-lisp-path "/usr/local/share/emacs/site-lisp/"))
 
 (my-add-path 'exec-path
 	     (list "~/bin"
+		   "/bin/"
+		   "/sbin/"
+		   "/usr/bin/"
+		   "/usr/sbin/"
 		   "/usr/local/bin"
 		   "/usr/local/sbin"
 		   "/opt/local/bin"
@@ -48,7 +37,7 @@
 		   "c:/cygwin/usr/local/sbin"
 		   "/usr/games"
 		   "/usr/X11R6/bin"
-		   "C:/Program Files/Mozilla Firefox"))
+		   "c:/program files/mozilla firefox"))
 
 (my-add-path 'Info-additional-directory-list
 	     (list "/opt/local/share/info"
@@ -57,54 +46,61 @@
 		   "c:/cygwin/usr/share/info"
 		   "c:/cygwin/usr/local/share/info"))
 
-(setq custom-file
-      (expand-file-name (concat my-lisp-path "/customize.el")))
+;;; networking.
+(defun get-ip-address ()
+  (interactive)
+  (let*
+      ((traceroute
+	(cond
+	 ((locate-library "traceroute" nil exec-path) "traceroute")
+	 ((locate-library "tracert" nil exec-path) "tracert")
+	 (t nil)))
+       
+       (return-value
+	(if traceroute
+	    (shell-command-to-string
+	     (concat traceroute " " system-name))
+	  "")))
+    
+    (string-match "[^0-9]*\\([0-9]+\\(\.[0-9]+\\)+\\)" return-value)
+    (match-string 1 return-value)))
+  
+(defun domestic-network-member-p ()
+  (let
+      ((domestic-address "^192.168.119.")
+       (domestic-domain-name "fascinating.local$"))
+    (and
+     (string-match domestic-address (get-ip-address))
+     (string-match domestic-domain-name system-name))))
 
-;; load libraries
+;; load essential libraries.
 (load "eval-safe.el")
 (load "autoload-if-found.el")
 (load "auto-save-buffers.el")
 (load "completions-highlight.el")
 (load "make-file-executable.el")
 
-;; load initial settings
-(load "init-auto-save-buffers.el")
-(load "init-browse-url.el")
-(load "init-browse-kill-ring.el")
-(load "init-css-mode.el")
-(load "init-ddskk.el")
-(load "init-develock.el")
-(load "init-dired-mode.el")
-(load "init-elscreen.el")
-(load "init-fonts.el")
-(load "init-frame.el")
-(load "init-howm.el")
-(load "init-html-helper-mode.el")
-(load "init-iswitchb.el")
-(load "init-language.el")
-(load "init-lookup.el")
-(load "init-navi2ch.el")
-(load "init-misc.el")
-(load "init-mmm-mode.el")
-(load "init-mpg123.el")
-(load "init-mwheel.el")
-(load "init-outline-mode.el")
-(load "init-psvn.el")
-(load "init-pukiwiki-mode.el")
-(load "init-riece.el")
-(load "init-ruby.el")
-(load "init-rails.el")
-(load "init-semi.el")
-(load "init-session.el")
-(load "init-shell-script.el")
-(load "init-server.el")
-(load "init-shimbun.el")
-(load "init-tdiary-mode.el")
-(load "init-tiarra-conf.el")
-(load "init-tramp.el")
-(load "init-w3m.el")
-(load "init-wget.el")
-(load "init-wl.el")
+;; load initialization files.
+(when (file-accessible-directory-p my-lisp-path)
+  (dolist (f (directory-files my-lisp-path))
+    (when (string-match "^init-.*el$" f)
+      (load (expand-file-name (concat my-lisp-path "/" f))))))
 
-(when (functionp 'howm-menu)
-  (howm-menu))
+(when (functionp 'howm-menu) (howm-menu))
+
+;; __END__
+
+;; fragments.
+(let*
+    ((domestic-router (shell-command-to-string "arp -a"))
+     (mac-addr
+      (progn
+	(string-match "\\([0-9A-Fa-f]+?[:-].*?\\)[\s]" domestic-router)
+	(match-string 1 domestic-router)))
+     (normalized-mac-addr "")
+     (temporary-char ""))
+  
+  (dotimes (i (length mac-addr) normalized-mac-addr)
+    (setq temporary-char (format "%c" (elt mac-addr i)))
+    (unless (string-match ":" temporary-char)
+      (setq normalized-mac-addr (concat normalized-mac-addr temporary-char)))))
