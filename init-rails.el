c9fda1068f03d1577d8b80bc93aa8f6090b031c7
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-09-26 21:05:05 +0000

    added key binds.

--- init-rails.el
+++ init-rails.el
@@ -1,16 +1,21 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
-(when 
+(when
     (and (require 'find-recursive nil t)
 	 (require 'snippet nil t)
 	 (locate-library "rails"))
   (defun try-complete-abbrev (old)
     (if (expand-abbrev) t nil))
-  
+
   (setq hippie-expand-try-functions-list
 	'(try-complete-abbrev
 	  try-complete-file-name
 	  try-expand-dabbrev))
-  
-  (require 'rails))
+
+  (require 'rails)
+  (define-key rails-minor-mode-map
+    "\C-c\C-p" 'rails-lib:run-primary-switch)
+  (define-key rails-minor-mode-map
+    "\C-c\C-n" 'rails-lib:run-secondary-switch))
+
