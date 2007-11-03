77e8f67be14fcf26bcc77d34ac74c96c2896c2f9
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-11-03 11:24:45 +0000

    - minor fix.

--- library/locate-executable.el
+++ library/locate-executable.el
@@ -3,16 +3,18 @@
 
 ;; based upon
 ;; http://d.hatena.ne.jp/elim/20071014/1192380471
+(require 'cl nil t)
+
 (defun locate-executable (basename)
   (let
       ((suffix (list nil ".exe" ".com" ".cmd" ".bat"))
        (return-value nil))
 
-    (dolist (s suffix)
+    (dolist (s suffix return-value)
       (setq return-value
 	    (or return-value
 		(locate-library (concat basename s) nil exec-path)))
-	(when return-value
-	  (unless (file-executable-p return-value)
-	    (setq return-value nil))))
-    return-value))
+      (when return-value
+	(if (file-executable-p return-value)
+	    (return return-value)
+	  (setq return-value nil))))))
