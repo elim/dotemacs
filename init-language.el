f8071a637692209a09710e70721d3076924ac7b0
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2007-11-03 11:33:39 +0000

    - use utf-translate-cjk-set-unicode-range on emacs22.

--- init-language.el
+++ init-language.el
@@ -1,6 +1,8 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
+(require 'cl nil t)
+
 ;;; Mule-UCS settings.
 (when (>= 21 emacs-major-version)
   (require 'un-define nil t) ; Unicode
@@ -38,3 +40,26 @@
 ;;; modified coding detection priority. (low => high)
 (dolist (c (list 'shift_jis 'iso-2022-jp 'euc-jp 'utf-8))
   (prefer-coding-system c))
+
+;;; http://www.pqrs.org/~tekezo/emacs/doc/wide-character/index.html
+(when (functionp 'utf-translate-cjk-set-unicode-range)
+  (utf-translate-cjk-set-unicode-range
+   '((#x00a2 . #x00a3)                    ; ¢, £
+     (#x00a7 . #x00a8)                    ; §, ¨
+     (#x00ac . #x00ac)                    ; ¬
+     (#x00b0 . #x00b1)                    ; °, ±
+     (#x00b4 . #x00b4)                    ; ´
+     (#x00b6 . #x00b6)                    ; ¶
+     (#x00d7 . #x00d7)                    ; ×
+     (#X00f7 . #x00f7)                    ; ÷
+     (#x0370 . #x03ff)                    ; Greek and Coptic
+     (#x0400 . #x04FF)                    ; Cyrillic
+     (#x2000 . #x206F)                    ; General Punctuation
+     (#x2100 . #x214F)                    ; Letterlike Symbols
+     (#x2190 . #x21FF)                    ; Arrows
+     (#x2200 . #x22FF)                    ; Mathematical Operators
+     (#x2300 . #x23FF)                    ; Miscellaneous Technical
+     (#x2500 . #x257F)                    ; Box Drawing
+     (#x25A0 . #x25FF)                    ; Geometric Shapes
+     (#x2600 . #x26FF)                    ; Miscellaneous Symbols
+     (#x2e80 . #xd7a3) (#xff00 . #xffef))))
