2e0be2939560a745ae915c3d32a43523e415bd09
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-01-19 04:14:56 +0000

    * wl/templates/default.el:   - Deleted unnecessary variable `smtp-open-connection-function'.

--- wl/templates/default.el
+++ wl/templates/default.el
@@ -5,7 +5,6 @@
  'wl-template-alist
  `(,(file-name-sans-extension
     (file-name-nondirectory load-file-name))
-   (smtp-open-connection-function . #'open-ssh-stream-idea)
    (wl-smtp-posting-server . "localhost")
    (wl-smtp-posting-port . 25)
    (wl-draft-send-mail-function . 'wl-draft-send-mail-with-smtp)
