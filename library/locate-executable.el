6b1d1e003956fc7b7fbfc8aa488ff3065685d6e7
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-01-24 18:44:40 +0000

    * library/locate-executable.el:   - dolist => mapcar.

--- library/locate-executable.el
+++ library/locate-executable.el
@@ -1,20 +1,16 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
-;; based upon
-;; http://d.hatena.ne.jp/elim/20071014/1192380471
-
-(defun locate-executable (name)
-  (if (boundp 'exec-suffixes) ;; emacs22 feature
-      (locate-file name exec-path exec-suffixes 'file-executable-p)
+(defun locate-executable (arg)
+  (if (and (boundp 'exec-suffixes) (fboundp 'locate-file)) ;; emacs22 feature
+      (locate-file arg exec-path exec-suffixes 'file-executable-p)
     (let
-	((exec-suffixes (list nil ".exe" ".com" ".cmd" ".bat"))
-	 (found nil))
-      (require 'cl nil t)
-      (dolist (s exec-suffixes found)
-	(unless found
-	  (setq found
-		(locate-library (concat name s) nil exec-path))
-	  (unless (and found (file-executable-p found))
-	    (setq found nil))))
-      found)))
+	((name arg)
+	 (exec-suffixes (list nil ".exe" ".com" ".cmd" ".bat")))
+      (car
+       (remove nil
+	       (mapcar
+		'(lambda (arg)
+		   (locate-library
+		    (concat name arg) nil exec-path)) exec-suffixes))))))
+
