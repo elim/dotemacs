569cf389177d3b4499802abd37298f18dd98a4a9
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-02-20 18:00:48 +0000

    init-auto-save-buffers.el:   * change interval.

--- init-auto-save-buffers.el
+++ init-auto-save-buffers.el
@@ -1,4 +1,4 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 (when (require 'auto-save-buffers nil t)
-  (run-with-idle-timer 2 t 'auto-save-buffers))
+  (run-with-idle-timer 0.5 t 'auto-save-buffers))
