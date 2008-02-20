124924a16e8ec2e567a042839fab106b7eff57c0
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-02-20 17:55:28 +0000

    init-language.el:   * refactoring. don't use Common Lisp extensions.

--- init-language.el
+++ init-language.el
@@ -1,65 +1,55 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
 
-(require 'cl nil t)
+;;; vars
+; default-buffer-file-coding-system
+; default-process-coding-system
+; default-file-name-coding-system
+; default-terminal-coding-system
+; default-keyboard-coding-system
+; coding-category-list
 
 ;;; Mule-UCS settings.
 (when (>= 21 emacs-major-version)
   (require 'un-define nil t) ; Unicode
   (require 'jisx0213 nil t)) ; JIS X 0213
 
-;;; UTF-8 environment.
-(defun setup-default-coding-systems (language-name target-list)
-  (let
-      ((my-default-coding-system language-name)
-       (funciion-name nil))
+(when (member '("utf-8-unix") coding-system-alist)
+  (set-language-environment 'utf-8)
 
-    (dolist (f target-list)
-      (setq funciion-name (intern (concat "set-" f)))
-      (when (functionp funciion-name)
-	(funcall funciion-name my-default-coding-system))
-      (set (intern f) my-default-coding-system))))
+  ;;; modified coding detection priority. (low => high)
+  (mapc  #'prefer-coding-system
+	 '(shift_jis iso-2022-jp euc-jp utf-8))
 
-(setup-default-coding-systems
- 'utf-8 (list
-	 "language-environment"))
+  ;;; http://www.pqrs.org/~tekezo/emacs/doc/wide-character/index.html
+  (when (functionp #'utf-translate-cjk-set-unicode-range)
+    (utf-translate-cjk-set-unicode-range
+     '((#x00a2 . #x00a3) ; ¢, £
+       (#x00a7 . #x00a8) ; §, ¨
+       (#x00ac . #x00ac) ; ¬
+       (#x00b0 . #x00b1) ; °, ±
+       (#x00b4 . #x00b4) ; ´
+       (#x00b6 . #x00b6) ; ¶
+       (#x00d7 . #x00d7) ; ×
+       (#X00f7 . #x00f7) ; ÷
+       (#x0370 . #x03ff) ; Greek and Coptic
+       (#x0400 . #x04FF) ; Cyrillic
+       (#x2000 . #x206F) ; General Punctuation
+       (#x2100 . #x214F) ; Letterlike Symbols
+       (#x2190 . #x21FF) ; Arrows
+       (#x2200 . #x22FF) ; Mathematical Operators
+       (#x2300 . #x23FF) ; Miscellaneous Technical
+       (#x2500 . #x257F) ; Box Drawing
+       (#x25A0 . #x25FF) ; Geometric Shapes
+       (#x2600 . #x26FF) ; Miscellaneous Symbols
+       (#x2e80 . #xd7a3) ; East Asian Scripts
+       (#xff00 . #xffef)))))
 
-(setup-default-coding-systems
- (cond
-  ((and (featurep 'mac-carbon)
-	(featurep 'utf-8m)) 'utf-8m)
-  ((featurep 'meadow)  'sjis-dos)
-  (t 'utf-8)) (list
-	       "default-coding-systems"
-	       "buffer-file-coding-system"
-	       "terminal-coding-system"
-	       "file-name-coding-system"
-	       "keyboard-coding-system"
-	       "clipboard-coding-system"))
-
-;;; modified coding detection priority. (low => high)
-(dolist (c (list 'shift_jis 'iso-2022-jp 'euc-jp 'utf-8))
-  (prefer-coding-system c))
-
-;;; http://www.pqrs.org/~tekezo/emacs/doc/wide-character/index.html
-(when (functionp 'utf-translate-cjk-set-unicode-range)
-  (utf-translate-cjk-set-unicode-range
-   '((#x00a2 . #x00a3)                    ; ¢, £
-     (#x00a7 . #x00a8)                    ; §, ¨
-     (#x00ac . #x00ac)                    ; ¬
-     (#x00b0 . #x00b1)                    ; °, ±
-     (#x00b4 . #x00b4)                    ; ´
-     (#x00b6 . #x00b6)                    ; ¶
-     (#x00d7 . #x00d7)                    ; ×
-     (#X00f7 . #x00f7)                    ; ÷
-     (#x0370 . #x03ff)                    ; Greek and Coptic
-     (#x0400 . #x04FF)                    ; Cyrillic
-     (#x2000 . #x206F)                    ; General Punctuation
-     (#x2100 . #x214F)                    ; Letterlike Symbols
-     (#x2190 . #x21FF)                    ; Arrows
-     (#x2200 . #x22FF)                    ; Mathematical Operators
-     (#x2300 . #x23FF)                    ; Miscellaneous Technical
-     (#x2500 . #x257F)                    ; Box Drawing
-     (#x25A0 . #x25FF)                    ; Geometric Shapes
-     (#x2600 . #x26FF)                    ; Miscellaneous Symbols
-     (#x2e80 . #xd7a3) (#xff00 . #xffef))))
+(when (and (not window-system)
+	   (string-equal "screen" (getenv "TERM")))
+  (and
+   (call-process "screen" nil nil nil "-X encoding euc")
+   (mapc #'(lambda (func)
+	     (funcall func 'euc-jp))
+	 (list #'set-terminal-coding-system
+	       #'set-keyboard-coding-system))))
