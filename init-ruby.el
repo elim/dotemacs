f585a4de40d9650aa8a8649ba901f48b8cde6efc
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-02-24 12:18:23 +0000

    init-ruby.el:   * fixed `irb' calling.

--- init-ruby.el
+++ init-ruby.el
@@ -3,17 +3,23 @@
 
 (when (require 'ruby-mode nil t)
   (when (require 'inf-ruby nil t)
-    (setq ruby-program-name  "ruby /usr/bin/irb --inf-ruby-mode"))
+    (let
+	((path (locate-library "irb" nil exec-path)))
+      (unless (file-executable-p path)
+	(setq ruby-program-name
+	      (mapconcat #'identity
+			 (list "ruby" path "--inf-ruby-mode") " ")))))
+
   (require 'ruby-electric nil t)
 
   (setq ruby-indent-level 2
 	ruby-indent-tabs-mode nil
 
 	auto-mode-alist
-	(append '(("\\.rb$" . ruby-mode)) auto-mode-alist)
+	(cons '("\\.rb$" . ruby-mode) auto-mode-alist)
 
 	interpreter-mode-alist
-	(append '(("ruby" . ruby-mode)) interpreter-mode-alist))
+	(cons '("ruby" . ruby-mode) interpreter-mode-alist))
 
   (add-hook 'ruby-mode-hook
 	    '(lambda ()
