5923a08009e905125bfacdc4db06a5f16be7b989
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-02-27 18:32:43 +0000

    init-align.el:   * refactoring. DRY!

--- init-align.el
+++ init-align.el
@@ -2,27 +2,24 @@
 ;; $Id$
 ;; based upon
 ;; http://d.hatena.ne.jp/rubikitch/20080227/1204051280
+
 (when (require 'align nil t)
+  ;; ruby-mode
   (mapc '(lambda (lst)
-	   (add-to-list 'align-rules-list lst))
+	   (add-to-list 'align-rules-list
+			(cons (car lst)
+			      (append (cdr lst)
+				      (list '(modes . '(ruby-mode)))))))
 	(list
-	 ;; ruby-mode
 	 '(ruby-comma-delimiter
-	   (regexp . ",\\(\\s-*\\)[^# \t\n]")
-	   (repeat . t)
-	   (modes  . '(ruby-mode)))
+	   (regexp . ",\\(\\s-*\\)[^# \t\n]"))
 
 	 '(ruby-hash-literal
-	   (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
-	   (repeat . t)
-	   (modes  . '(ruby-mode)))
+	   (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]"))
 
 	 '(ruby-assignment-literal
-	   (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
-	   (repeat . t)
-	   (modes  . '(ruby-mode)))
+	   (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]"))
 
-	 '(ruby-xmpfilter-mark ;;TODO add to rcodetools.el
+	 '(ruby-xmpfilter-mark ;;TODO: add to rcodetools.el
 	   (regexp . "\\(\\s-*\\)# => [^#\t\n]")
-	   (repeat . nil)
-	   (modes  . '(ruby-mode))))))
+	   (repeat . nil))))
