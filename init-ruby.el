c9ba6beaf4411b19f6753b46f7eef3ba43cc64a2
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-02-27 18:35:06 +0000

    init-ruby.el:   * changed load priorilty.

--- init-ruby.el
+++ init-ruby.el
@@ -3,19 +3,21 @@
 
 (when (require 'ruby-mode nil t)
   (require 'ruby-electric nil t)
-  (let
-      ((ruby (locate-executable "ruby"))
-       (irb (locate-library "irb" nil exec-path))
-       (arg "--inf-ruby-mode -Ku"))
-    (when (and irb (not (file-executable-p irb)))
-      (and
-       (setq ruby-program-name
-	     (mapconcat #'identity
-			(list ruby irb arg) " "))
-       (when (require 'inf-ruby nil t)
-	 (define-key inferior-ruby-mode-map
-	   "\C-\M-p"  #'comint-previous-input)))))
-
+  (when (require 'inf-ruby nil t)
+    (let
+	((ruby (locate-executable "ruby"))
+	 (irb (locate-library "irb" nil exec-path))
+	 (arg "--inf-ruby-mode -Ku"))
+      (when (and irb (not (file-executable-p irb)))
+	(and
+	 (setq ruby-program-name
+	       (mapconcat #'identity
+			  (list ruby irb arg) " "))))
+      (mapc '(lambda (lst)
+	       (apply #'define-key inferior-ruby-mode-map lst))
+	    (list
+	     '("\C-p"  comint-previous-input)
+	     '("\C-n"  comint-next-input)))
 
   (setq ruby-indent-level 2
 	ruby-indent-tabs-mode nil
