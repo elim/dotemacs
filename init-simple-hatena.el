fb5e8eb32737f18cc5b45d736946bd616d2d9730
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-08 16:41:00 +0000

    init-simple-hatena.el:   - set time offset.   - set default Hatena id.   - set default Hatena Group name.   - set debug flag.   - disabled timestamp.

--- init-simple-hatena.el
+++ init-simple-hatena.el
@@ -4,44 +4,14 @@
 ;; based upon
 ;; http://coderepos.org/share/wiki/SimpleHatenaMode
 
-(when
-    (require 'simple-hatena-mode nil t)
-
+(when (require 'simple-hatena-mode nil t)
   (when (require 'hatenahelper-mode nil t)
     (add-hook 'simple-hatena-mode-hook
 	      '(lambda ()
-		 (hatenahelper-mode 1)))))
+		 (hatenahelper-mode 1))))
 
-  ;; location of hatena diary writer (default: hw.pl)
-  ;; (setq simple-hatena-bin "hw.pl")
-
-  ;; data sotre directory (default: ~/.hatena)
-  ;; (setq simple-hatena-root "~/.hatena")
-
-  ;; default hatena ID (default: nil)
-  ;; (setq simple-hatena-default-id "antipop")
-
-  ;; default hatena group name (default: nil)
-  ;; (setq simple-hatena-default-group "subtech")
-
-  ;; user agent (default: simple-hatena-mode/version)
-  ;; (setq simple-hatena-option-useragent "Hatena::Diary::Writer")
-
-  ;; insert time stamp in a permanent link (default: t)
-  ;; (setq simple-hatena-use-timestamp-permalink-flag nil)
-
-  ;; 日付を計算する際に用いるオフセット。たとえば以下のように6に設定する
-  ;; と、午前6時まで前日の日付として扱われる(デフォルト値: nil)
-  ;; (setq simple-hatena-time-offset 6)
-
-  ;; debug mode (default: nil)
-  ;; (setq simple-hatena-option-debug-flag t)
-
-  ;; timeout seconds (default: 30)
-  ;; (setq simple-hatena-option-timeout 60)
-
-  ;; use cookie (default: t)
-  ;; (setq simple-hatena-option-cookie-flag "nil")
-
-  ;; process buffer name (default:: *SimpleHatena*)
-  ;; (setq simple-hatena-process-buffer-name "*OtherBufferName*")
+  (setq simple-hatena-default-id "elim"
+	simple-hatena-default-group "elim"
+	simple-hatena-use-timestamp-permalink-flag nil
+	simple-hatena-time-offset 6
+	simple-hatena-option-debug-flag t))
