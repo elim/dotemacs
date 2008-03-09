d7f97b6ed96ee350ba0cc99d786a75c83e7267b5
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-09 15:52:33 +0000

    init-simple-hatena.el:   - forced utf-8 writing.

--- init-simple-hatena.el
+++ init-simple-hatena.el
@@ -5,13 +5,20 @@
 ;; http://coderepos.org/share/wiki/SimpleHatenaMode
 
 (when (require 'simple-hatena-mode nil t)
+  (setq simple-hatena-default-id "elim"
+	simple-hatena-default-group "elim"
+	simple-hatena-use-timestamp-permalink-flag nil
+	simple-hatena-time-offset 6
+	simple-hatena-option-debug-flag t)
+
   (when (require 'hatenahelper-mode nil t)
     (add-hook 'simple-hatena-mode-hook
 	      '(lambda ()
 		 (hatenahelper-mode 1))))
 
-  (setq simple-hatena-default-id "elim"
-	simple-hatena-default-group "elim"
-	simple-hatena-use-timestamp-permalink-flag nil
-	simple-hatena-time-offset 6
-	simple-hatena-option-debug-flag t))
+  (when (member '("utf-8-unix") coding-system-alist)
+    (mapc #'(lambda (arg)
+	      (add-hook aUrg
+			(lambda ()
+			  (setq buffer-file-coding-system 'utf-8-unix))))
+	  (list 'howm-view-open-hook 'howm-create-file-hook)))
