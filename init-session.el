543d13259b4fb8b20a1e8e8594aa37e5b7fe5378
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-16 03:43:35 +0000

    init-session.el:   - saved places.

--- init-session.el
+++ init-session.el
@@ -1,9 +1,10 @@
 ;; -*- emacs-lisp -*-
 ;; $Id$
 
+;; http://d.hatena.ne.jp/higepon/20061230/1167447339
 (when (require 'session nil t)
   (setq history-length t)
-  (setq session-initialize '(de-saveplace session keys menus))
+  (setq session-initialize '(de-saveplace session keys menus places))
   (setq session-globals-include '((kill-ring 255)
 				  (session-file-alist 255 t)
 				  (file-name-history 255)))
