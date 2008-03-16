bfd84572865681e3645ba1f73a48a959ca6f80ad
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-16 03:47:05 +0000

    init-tramp.el   - set default method `sshx'.   - allow multi-hop methos `sshx'.

--- init-tramp.el
+++ init-tramp.el
@@ -1,10 +1,15 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$
-;; based upon
-;;   http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=175346
-;;   http://yoichi.geiin.org/d/?date=20030328#p01
 
 (when (require 'tramp nil t)
+  ;; http://aligach.net/diary/20071022.html
+  (setq tramp-default-method "sshx")
+  (add-to-list
+   'tramp-multi-connection-function-alist
+   '("sshx" tramp-multi-connect-rlogin "ssh -t -t %h -l %u /bin/sh%n"))
+
+  ;; http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=175346
+  ;; http://yoichi.geiin.org/d/?date=20030328#p01
   (setq auto-save-file-name-transforms
 	`(("\\`/[^/]*:\\(.+/\\)*\\(.*\\)"
 	   ,(expand-file-name "\\2" temporary-file-directory)))))
