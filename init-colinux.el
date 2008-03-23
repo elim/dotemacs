d5a14c178ee04f0dd8d62fa574a18bfd448fd929
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-23 13:00:22 +0000

    init-colinux.el   - initial.

---
+++ init-colinux.el
@@ -0,0 +1,40 @@
+;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: t -*-
+;; $Id$
+
+(when (and (locate-executable "dmesg")
+	   (with-temp-buffer
+	     (call-process "dmesg" nil t nil)
+	     (goto-char (point-min))
+	     (if (re-search-forward "^\\(cofuse\\)\.+" nil t)
+		 (match-string 1))))
+
+  (defvar clipboard-file-name "~/media/home/tmp/clipboard")
+
+  (defadvice kill-new (after throw-clipboard activate)
+      (let
+	  ((str (car kill-ring))
+	   (file (expand-file-name clipboard-file-name)))
+
+	(when (file-writable-p file)
+	   (with-temp-buffer
+	     (insert str)
+	     (set-buffer-file-coding-system 'sjis-dos)
+	     (write-region (point-min) (point-max)
+			   file nil 'no-message)))))
+
+  (defvar clipboard-synchronize-interval 0.5)
+  (defvar clipboard-mtime '(0 . 0))
+  (run-with-idle-timer
+   clipboard-synchronize-interval
+   'repeat
+   '(lambda ()
+      (let*
+	  ((file (expand-file-name clipboard-file-name))
+	   (current-mtime (nth 5 (file-attributes file))))
+
+	(unless (equal clipboard-mtime current-mtime)
+	  (and
+	   (setq clipboard-mtime current-mtime)
+	   (with-temp-buffer
+	     (insert-file-contents file)
+	     (kill-ring-save (point-min) (point-max)))))))))
