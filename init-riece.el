ba7f0d63b313103c8704a92f528ecaa00d8ccdd0
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-27 15:46:11 +0000

    init-riece.el:  - refactoring.

--- init-riece.el
+++ init-riece.el
@@ -1,22 +1,19 @@
-;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
+;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: t -*-
 ;; $Id$
 
 (when (autoload-if-found 'riece "riece" nil t)
-
-  ;; server settings
   (setq riece-server-alist
 	'(("default"
-	  :host "localhost"
-	  :service 6667
-	  :coding utf-8))
+	   :host "localhost"
+	   :service 6667
+	   :coding utf-8))
 	riece-server "default"
 	riece-ndcc-server-address "localhost"
-	riece-channel-buffer-mode 't
-	riece-user-list-buffer-mode 't
-	riece-layout '"middle-left")
+	riece-channel-buffer-mode t
+	riece-user-list-buffer-mode t
+	riece-layout "middle-left"
 
-  ;; fils and directories
-  (setq riece-preference-directory
+	riece-preference-directory
 	(expand-file-name "riece" base-directory)
 
 	riece-addon-directory
@@ -26,44 +23,39 @@
 	(expand-file-name "save" riece-preference-directory)
 
 	riece-variables-file
-	(expand-file-name "init" riece-preference-directory))
+	(expand-file-name "init" riece-preference-directory)
 
-  (setq riece-variables-files
-	(list riece-variables-file
-	      riece-saved-variables-file))
+	riece-variables-files (list riece-variables-file
+				    riece-saved-variables-file)
 
-  (setq riece-saved-forms
-	'(riece-channel-buffer-mode
-	  riece-others-buffer-mode
-	  riece-user-list-buffer-mode
-	  riece-channel-list-buffer-mode
-	  riece-layout riece-addons))
+	riece-saved-forms '(riece-channel-buffer-mode
+			    riece-others-buffer-mode
+			    riece-user-list-buffer-mode
+			    riece-channel-list-buffer-mode
+			    riece-layout riece-addons)
 
-  (setq riece-addons
-	'(riece-alias
-	  riece-button
-	  riece-ctcp riece-guess riece-highlight
-	  riece-history riece-icon riece-keyword
-	  riece-menu riece-skk-kakutei riece-unread
-	  riece-url))
+	riece-addons '(riece-alias
+		       riece-button
+		       riece-ctcp riece-guess riece-highlight
+		       riece-history riece-icon riece-keyword
+		       riece-menu riece-skk-kakutei riece-unread
+		       riece-url)
 
-  (setq riece-keywords '("Elim" "elim"
-			 "えりむ" "エリム"
-			 "えろり" "えろむ"
-			 "lisp" "emacs"))
+	riece-keywords '(("[Ee]lim" . 0)
+			 ("[Ee]macs" . 0)))
 
   (let
       ((notify-sound-file (expand-file-name "~/sounds/notify.wav"))
        (notify-sound-player "mplayer"))
 
-    (when (and (locate-executable notify-sound-player)
-	       (file-exists-p notify-sound-file))
-       (add-hook 'riece-notify-keyword-functions
-		 `(lambda (keyword)
-		    (start-process keyword "*Messages*" ,notify-sound-player
-				   (expand-file-name ,notify-sound-file))))))
-
+    (and (locate-executable notify-sound-player)
+	 (file-exists-p notify-sound-file)
+	 (add-hook 'riece-notify-keyword-functions
+		   `(lambda (keyword)
+		      (start-process keyword "*Messages*" ,notify-sound-player
+				     (expand-file-name ,notify-sound-file))))))
   (add-hook 'riece-startup-hook
-	    (lambda ()
-	      (define-key riece-command-mode-map "\C-xn"
-		'riece-command-enter-message-as-notice))))
+	    '(lambda ()
+	       (define-key riece-command-mode-map [(control x) n]
+		 'riece-command-enter-message-as-notice))))
+
