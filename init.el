f1b6683304fca1fb5f6fb09ca22ab9042d80bc1b
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-30 17:54:05 +0000

    init.el:   - refactoring.   - added function merge-path-without-duplicate.   - move function get-ip-address to library/networks.el

--- init.el
+++ init.el
@@ -1,52 +1,40 @@
-;;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
+;;; -*- coding: utf-8; mode: emacs-lisp; indent-tabs-mode: t -*-
 ;;; $Id$
 
-;;; provisional measures for cygwin $PWD environment variable.
-(when
-    (not (file-directory-p default-directory))
-  (setq default-directory (getenv "HOME")))
+;; provisional measures for cygwin $PWD environment variable.
+(and (not (file-directory-p default-directory))
+     (setq default-directory (getenv "HOME")))
 
-;;; path and filenames.
-(setq base-directory "~/.emacs.d")
-(setq preferences-directory "~/.emacs.d")
-(setq libraries-directory
-  (expand-file-name "library" base-directory))
+;; path and filenames.
+(setq base-directory "~/.emacs.d"
+      preferences-directory "~/.emacs.d"
+      libraries-directory (expand-file-name "library" base-directory)
 
-(setq custom-file
+      custom-file
       (expand-file-name "customize.el" base-directory))
 
-(defmacro forced-list-transform (arg)
-  (list 'condition-case 'err
-	(list 'if (list 'and (list 'boundp arg)(list 'listp arg))
-	      arg
-	      (list 'list arg))
-	(list 'error
-	      (list 'cond
-		    (list (list 'equal (list 'car 'err) ''void-variable)
-			  (list 'list))
-		    (list (list 'and
-				(list 'equal (list 'car 'err)
-				      ''wrong-type-argument)
-				(list 'atom arg))
-			  (list 'list arg))
-		    (list (list 'and
-				(list 'equal (list 'car 'err)
-				      ''wrong-type-argument)
-				(list 'listp arg))
-			  arg)
-		    (list t (list 'message "%s" 'err))))))
-
-(defun marge-lists-without-duplicate (base additional)
-  (let (result)
-    (if additional
-	(mapc '(lambda (arg)
-		 (setq result (cons arg
-				    (remove arg base)))) additional)
-      (setq result base))
-    result))
+(defun merge-path-without-duplicate (&rest path)
+  (let (lst)
+    (mapc '(lambda (seq)
+	     (setq lst (append lst seq)))
+	  (mapcar '(lambda (p)
+		     (cond
+		      ((listp p) p)
+		      ((stringp p) (list p))
+		      (t nil)))
+		  path))
+    (mapc '(lambda (seq)
+	     (setq lst
+		   (cond
+		    ((file-accessible-directory-p seq)
+		     (cons seq (remove seq lst)))
+		    (t
+		     (remove seq lst)))))
+	  (reverse (remove nil lst)))
+    lst))
 
 (setq load-path
-      (marge-lists-without-duplicate
+      (merge-path-without-duplicate
        load-path
        (list base-directory
 	     preferences-directory
@@ -54,7 +42,7 @@
 	     "/usr/local/share/emacs/site-lisp/")))
 
 (setq exec-path
-      (marge-lists-without-duplicate
+      (merge-path-without-duplicate
        exec-path
        (list "~/bin"
 	     "/bin/"
@@ -77,8 +65,7 @@
 	     "c:/program files/mozilla firefox")))
 
 (setq Info-additional-directory-list
-      (marge-lists-without-duplicate
-       (forced-list-transform Info-additional-directory-list)
+      (merge-lists-without-duplicate
        (list "/Applications/Emacs.app/Contents/Resources/info/"
 	     "/opt/local/share/info"
 	     "/sw/info"
@@ -86,49 +73,17 @@
 	     "c:/cygwin/usr/share/info"
 	     "c:/cygwin/usr/local/share/info")))
 
-;;; networking.
-(defun get-ip-address ()
-  (interactive)
-  (let
-      ((traceroute
-	(or
-	 (locate-library "traceroute" nil exec-path)
-	 (locate-library "tracert.exe" nil exec-path)))
-       (host-name
-	(if (string-match "\\." system-name)
-	    (progn
-	      (string-match "^\\(.+?\\)\\." system-name)
-	      (match-string-no-properties 1 system-name))
-	  system-name)))
-
-    (if traceroute
-	(with-temp-buffer
-	  (call-process traceroute nil t nil host-name)
-	  (goto-char (point-min))
-	  (if (re-search-forward "[^0-9]*\\([0-9]+\\(\.[0-9]+\\)+\\)" nil t)
-	      (match-string 1)))
-      "unknown")))
-
-(defun domestic-network-member-p ()
-  (let
-      ((domestic-address "^192.168.119.")
-       (domestic-domain-name "fascinating.local$"))
-    (and
-     (string-match domestic-address (get-ip-address))
-     (string-match domestic-domain-name system-name))))
-
-
 (defun load-directory-files (dir &optional regex)
   (let*
       ((regex (or regex ".+"))
        (files (and
-	       dir
 	       (file-accessible-directory-p dir)
 	       (directory-files dir 'full regex))))
 
     (mapc #'(lambda (file)
-	       (when (load file nil t)
-		 (message "`%s' loaded." file))) files)))
+	      (when (load file nil t)
+		(message "`%s' loaded." file))) files)))
+
 
 ;; load essential libraries.
 (load-directory-files libraries-directory "^.+el$")
