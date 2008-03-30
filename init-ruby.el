8559c35852889071a6718efec342ac7e0873b776
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-30 17:54:30 +0000

    init-ruby.el:   - refactoring.

--- init-ruby.el
+++ init-ruby.el
@@ -1,43 +1,35 @@
-;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
-;; $Id$
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: t -*-
+;;; $Id$
 
-(when (require 'ruby-mode nil t)
-  (setq ruby-indent-level 2
-	ruby-indent-tabs-mode nil
-	ruby-indnt-paren-style nil)
+(and (require 'ruby-mode nil t)
+     (setq ruby-indent-level 2
+	   ruby-indent-tabs-mode nil
+	   ruby-indnt-paren-style nil)
 
-  (mapc '(lambda (lst)
-	   (add-to-list 'auto-mode-alist lst))
-	(list '("\\.rb$" . ruby-mode)
-	      '("Rakefile" . ruby-mode)))
+     (mapc '(lambda (lst)
+	      (add-to-list 'auto-mode-alist lst))
+	   (list '("\\.rb$" . ruby-mode)
+		 '("Rakefile" . ruby-mode)))
 
-  (when (require 'ruby-electric nil t)
-    (setq ruby-electric-expand-delimiters-list '(?\{))
-    (add-hook 'ruby-mode-hook
-	      '(lambda ()
-		 (ruby-electric-mode 1))))
+     (and (require 'ruby-electric nil t)
+	  (setq ruby-electric-expand-delimiters-list '(?\{))
+	  (add-hook 'ruby-mode-hook
+		    '(lambda ()
+		       (ruby-electric-mode 1))))
 
-  (when (require 'inf-ruby nil t)
-    (setq interpreter-mode-alist
-	  (cons '("ruby" . ruby-mode) interpreter-mode-alist))
-    (let
-	((ruby (locate-executable "ruby"))
-	 (irb (locate-library "irb" nil exec-path))
-	 (args (list "--inf-ruby-mode" "-Ku")))
+     (and (require 'inf-ruby nil t)
+	  (setq interpreter-mode-alist
+		(cons '("ruby" . ruby-mode) interpreter-mode-alist))
+	  (let
+	      ((ruby (locate-executable "ruby"))
+	       (irb (locate-library "irb" nil exec-path))
+	       (args (list "--inf-ruby-mode" "-Ku")))
 
-      (and irb
-	   (setq ruby-program-name
-		 (mapconcat #'identity
-			    `(,ruby ,irb ,@args) " "))
+	    (and irb
+		 (setq ruby-program-name
+		       (mapconcat #'identity
+				  `(,ruby ,irb ,@args) " "))
 
-	   (add-hook 'ruby-mode-hook
-		     '(lambda ()
-			(inf-ruby-keys)))
-
-	   (add-hook 'inferior-ruby-mode-hook
-		     '(lambda ()
-			(mapc '(lambda (pair)
-				 (apply #'define-key (current-local-map) pair))
-			      (list
-			       '([control p] comint-previous-input)
-			       '([control n] comint-next-input)))))))))
+		 (add-hook 'ruby-mode-hook
+			   '(lambda ()
+			      (inf-ruby-keys)))))))
