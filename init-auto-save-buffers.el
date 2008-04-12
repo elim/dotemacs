3e3efe3584dd1f25375b7fbb463605e574e48329
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-04-12 12:58:03 +0000

    auto-save-buffers.el:   - いくつかの実装/改良版が単体ファイルとして入手できる。自由に選択できるようリポジトリから削除する。
    init-auto-save-buffers.el:
      - それらの入手先を記載した。

--- init-auto-save-buffers.el
+++ init-auto-save-buffers.el
@@ -1,4 +1,10 @@
-;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
-;; $Id$
-(when (require 'auto-save-buffers nil t)
-  (run-with-idle-timer 0.5 t 'auto-save-buffers))
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: t -*-
+;;; $Id$
+
+;; variants of auto-save-buffers.
+;; http://0xcc.net/unimag/12/
+;; http://0xcc.net/misc/auto-save/auto-save-buffers.el
+;; http://homepage3.nifty.com/oatu/emacs/archives/auto-save-buffers.el
+
+(and (require 'auto-save-buffers nil t)
+     (run-with-idle-timer 0.5 t 'auto-save-buffers))
