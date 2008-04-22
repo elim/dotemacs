bd1a454ebb1ae7c5823ec1bc6faf5e43ce017987
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-04-22 13:33:19 +0000

    init-psvn.el:   - svn(1) へ utf-8 を渡すように process-coding-system-alist を変更。

--- init-psvn.el
+++ init-psvn.el
@@ -1,12 +1,13 @@
-;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
-;; $Id$
+;;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
+;;; $Id$
 
 (when (require 'psvn nil t)
-  (setq svn-status-hide-unmodified t)
-  (setq process-coding-system-alist
-	(cons '("svn" . utf-8) process-coding-system-alist))
+  (setq svn-status-hide-unmodified t
+	process-coding-system-alist (cons '("svn" . utf-8-unix)
+					  process-coding-system-alist)
+	svn-status-coding-system 'utf-8-unix)
+
   (add-hook 'dired-mode-hook
 	    '(lambda ()
-	       ;;(define-key dired-mode-map "V" 'cvs-examine)
 	       (define-key dired-mode-map "V" 'svn-status)
 	       (turn-on-font-lock))))
