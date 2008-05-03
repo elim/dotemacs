2f002dcaf66127c40e411bd29c6e4cb9ba2eb817
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-05-03 02:01:17 +0000

    init-mmm.el:   - settings for .html.erb

--- init-mmm.el
+++ init-mmm.el
@@ -1,32 +1,27 @@
 ;; -*- mode: emacs-lisp; coding: utf-8-unix -*-
 ;; $Id$;
 
-(when (locate-library "mmm-mode")
-  (require 'mmm-mode)
-  ;;(setq mmm-global-mode t)
-  (setq mmm-global-mode 'maybe)
+(when (require 'mmm-mode nil t)
+  (setq mmm-global-mode 'maybe
+	mmm-submode-decoration-level 2)
 
-  ;; mmm-modeをカラフルに
-  (setq mmm-submode-decoration-level 2)
-  
-  ;; mmm-modeの前景色と背景色を入れ換える。
-  ;;(invert-face 'mmm-default-submode-face)
+  ;(invert-face 'mmm-default-submode-face)
 
-  ;; mmm-modeのフェイスを変更
-  ;;(set-face-bold-p 'mmm-default-submode-face t)
-  ;;(set-face-background 'mmm-default-submode-face "White")
+  ;(set-face-bold-p 'mmm-default-submode-face t)
+  ;(set-face-background 'mmm-default-submode-face "White")
 
-  ;; 背景を無くする
-  (progn
-    (set-face-background 'mmm-default-submode-face nil)
-    (set-face-background 'mmm-cleanup-submode-face nil)
-    (set-face-background 'mmm-code-submode-face nil)
-    (set-face-background 'mmm-comment-submode-face nil)
-    (set-face-background 'mmm-declaration-submode-face nil)
-    (set-face-background 'mmm-init-submode-face nil)
-    (set-face-background 'mmm-output-submode-face nil)
-    (set-face-background 'mmm-special-submode-face nil))
-  
+  (mapc
+    '(lambda (arg)
+       (set-face-background arg nil))
+    '(mmm-default-submode-face
+      mmm-cleanup-submode-face
+      mmm-code-submode-face
+      mmm-comment-submode-face
+      mmm-declaration-submode-face
+      mmm-init-submode-face
+      mmm-output-submode-face
+      mmm-special-submode-face))
+
   ;; eRuby
   (mmm-add-mode-ext-class nil "\\.erb?\\'" 'html-erb)
   (mmm-add-mode-ext-class nil "\\.rhtml" 'html-erb)
