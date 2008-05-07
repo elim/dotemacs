4173a1fbcbed6ad5424cafea05eae6f4a36bc0e9
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-05-07 03:57:07 +0000

    init-psvn.el:   - (setenv "LANG" "en_US.UTF-8") で済んだようなので process-coding-system-alist の設定を削除。

--- init-psvn.el
+++ init-psvn.el
@@ -3,8 +3,6 @@
 
 (when (require 'psvn nil t)
   (setq svn-status-hide-unmodified t
-	process-coding-system-alist (cons '("svn" . utf-8-unix)
-					  process-coding-system-alist)
 	svn-status-coding-system 'utf-8-unix)
 
   (add-hook 'dired-mode-hook
