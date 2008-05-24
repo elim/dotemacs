64f71f96613900b4dad922035b34322d6f1e48a4
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-05-24 01:52:04 +0000

    reduce indent size.

--- init-css.el
+++ init-css.el
@@ -3,6 +3,6 @@
 
 (when (autoload-if-found 'css-mode "css-mode" nil t nil)
   (setq auto-mode-alist
-	(cons '("\\.css\\'" . css-mode) auto-mode-alist))
-  (setq cssm-indent-function #'cssm-c-style-indenter))
-
+	(cons '("\\.css\\'" . css-mode) auto-mode-alist)
+	cssm-indent-function #'cssm-c-style-indenter
+	cssm-indent-level 2))
