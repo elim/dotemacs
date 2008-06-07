5cdd1d1badb21a30b3b6c12c3b874a25754d3578
Takeru Naito <takeru.naito@gmail.com>
2008-06-07 16:35:28 +0900

    press 'V' to git-status. on dired-mode.

--- init-psvn.el
+++ init-psvn.el
@@ -3,9 +3,4 @@
 
 (when (require 'psvn nil t)
   (setq svn-status-hide-unmodified t
-        svn-status-coding-system 'utf-8-unix)
-
-  (add-hook 'dired-mode-hook
-            '(lambda ()
-               (define-key dired-mode-map "V" 'svn-status)
-               (turn-on-font-lock))))
+        svn-status-coding-system 'utf-8-unix))
