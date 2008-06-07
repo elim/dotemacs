14bcc516da5485bc75a6577f4a60d7b764b3eaec
Takeru Naito <takeru.naito@gmail.com>
2008-06-07 16:34:56 +0900

    initial commit.

---
+++ init-git.el
@@ -0,0 +1,14 @@
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
+;;; $Id$
+
+(when (require 'git nil t)
+  (autoload-if-found 'git-blame-mode "git-blame"
+                     "Minor mode for incremental blame for Git." t)
+
+  (when (locate-library "vc-git")
+    (add-to-list 'vc-handled-backends 'GIT))
+
+  (add-hook 'dired-mode-hook
+            '(lambda ()
+               (define-key dired-mode-map "V" 'git-status)
+               (turn-on-font-lock))))
