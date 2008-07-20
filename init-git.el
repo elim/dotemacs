b318d5275a0c0f311ea04cf95e8a3aad51baec59
Takeru Naito <takeru.naito@gmail.com>
2008-07-20 22:56:07 +0900

    added function 'git-sync'.

--- init-git.el
+++ init-git.el
@@ -1,5 +1,4 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
 (when (require 'git nil t)
   (autoload-if-found 'git-blame-mode "git-blame"
@@ -12,3 +11,17 @@
             '(lambda ()
                (define-key dired-mode-map "V" 'git-status)
                (turn-on-font-lock))))
+
+(when (locate-executable "git")
+  (defmacro git (&rest arg)
+    `(call-process "git" nil t nil ,@arg))
+
+  (defun git-sync (directory)
+    (with-temp-buffer ;; preserve pwd
+      (cd directory)
+      (git "add" ".")
+      (git "pull")
+      (git "commit" "-m" (format-time-string
+                          "Snapshot %b.%d,%Y(%a) %H:%M:%S"))
+      (git "push"))))
+
