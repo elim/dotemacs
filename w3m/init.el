24ecbba4da3af72bf9dbc9eea4d514aa3f834d9a
Takeru Naito <takeru.naito@gmail.com>
2008-09-26 22:36:46 +0900

    (setq w3m-coding-system 'utf-8-unix)

--- w3m/init.el
+++ w3m/init.el
@@ -1,11 +1,10 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
-(setq w3m-pop-up-frames nil)
-(setq w3m-tab-width 4)
-(setq w3m-use-cookies t)
-(setq w3m-use-tab t)
-(setq w3m-use-toolbar t)
-(setq w3m-home-page "about:blank")
-(setq w3m-weather-default-area "道央・石狩")
-
+(setq w3m-coding-system 'utf-8-unix
+      w3m-pop-up-frames nil
+      w3m-tab-width 4
+      w3m-use-cookies t
+      w3m-use-tab t
+      w3m-use-toolbar t
+      w3m-home-page "about:blank"
+      w3m-weather-default-area "道央・石狩")
