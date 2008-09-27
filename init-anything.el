6c22185996610db5970718c3910ce7a9f17d9537
Takeru Naito <takeru.naito@gmail.com>
2008-09-27 12:40:56 +0900

    Specify anything sources.

--- init-anything.el
+++ init-anything.el
@@ -1,13 +1,24 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
 (when (and (require 'anything nil t)
            (require 'anything-config nil t))
-  (mapc '(lambda (key)
-           (global-set-key key 'anything))
-        (list
-         [(control ?:)]
-         [(control \;)]
-         [(control x)(b)]
-         [(control x)(control :)]
-         [(control x)(control \;)])))
+
+  (when (require 'anything-complete nil t)
+    (anything-lisp-complete-symbol-set-timer 150)
+
+    (setq anything-sources
+          (list anything-c-source-buffers
+                anything-c-source-file-name-history
+                anything-c-source-info-pages
+                anything-c-source-man-pages
+                anything-c-source-locate
+                anything-c-source-emacs-commands))
+
+    (mapc '(lambda (key)
+             (global-set-key key 'anything))
+          (list
+           [(control ?:)]
+           [(control \;)]
+           [(control x)(b)]
+           [(control x)(control :)]
+           [(control x)(control \;)]))))
