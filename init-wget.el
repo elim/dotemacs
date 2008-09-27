f620551b409b50eaf4ae24c901b31d84b5cfdfca
Takeru Naito <takeru.naito@gmail.com>
2008-09-27 12:43:00 +0900

    Specify download directory.

--- init-wget.el
+++ init-wget.el
@@ -1,7 +1,9 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
-(autoload-if-found
- 'wget "wget" "wget interface for Emacs." t)
-(autoload-if-found
- 'wget-web-page "wget" "wget interface to download whole web page." t)
+(when (autoload-if-found
+       'wget "wget" "wget interface for Emacs." t)
+  (autoload-if-found
+   'wget-web-page "wget" "wget interface to download whole web page." t)
+
+  (setq wget-download-directory
+        (expand-file-name "~/Downloads")))
