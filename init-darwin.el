30c59d6464a0a2de9427c76f33fbdb8f4ed4330b
Takeru Naito <takeru.naito@gmail.com>
2008-09-28 14:36:58 +0900

    Finished the duty. (Measure of KeyRemap4MacBook).

--- init-darwin.el
+++ init-darwin.el
@@ -1,47 +1,7 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
-;; carbon emacs
-(setq mac-pass-control-to-system nil
-      mac-pass-command-to-system nil
-      mac-option-modifier 'meta)
-
-(when (let
-          ((exec "sysctl")
-           (arg "keyremap4macbook.option.emacsmode_controlPNBF"))
-
-        (and (locate-executable exec)
-             (string-equal "1"
-                    (with-temp-buffer
-                      (call-process exec nil t nil arg)
-                      (goto-char (point-min))
-                      (if (re-search-forward "\\(.\\)$" nil t)
-                          (match-string 0))))))
-  (and
-   (global-set-key [(control x) (right)] #'find-file)
-
-   (defadvice define-key (around keyreremap (keymap key def)
-                                 activate)
-     ad-do-it
-
-     (setq key
-           (cond
-            ((vectorp key) (apply 'vector
-                                  (mapcar
-                                   #'(lambda (k)
-                                       (cond
-                                        ((equal "\C-p" k) '(up))
-                                        ((equal "\C-n" k) '(down))
-                                        ((equal "\C-b" k) '(left))
-                                        ((equal "\C-f" k) '(right))
-                                        (t k)))
-                                   key)))
-            ((stringp key) (cond
-                            ((equal "\C-p" key) [(up)])
-                            ((equal "\C-n" key) [(down)])
-                            ((equal "\C-b" key) [(left)])
-                            ((equal "\C-f" key) [(right)])
-                            (t key)))
-            (t key)))
-
-     ad-do-it)))
+;; Carbon Emacs
+(when (eq window-system 'mac)
+  (setq mac-pass-control-to-system nil
+        mac-pass-command-to-system nil
+        mac-option-modifier 'meta)
