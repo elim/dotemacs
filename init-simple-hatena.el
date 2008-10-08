c74e7aeb11efcd27ed26b394ea39e63d990965e8
Takeru Naito <takeru.naito@gmail.com>
2008-10-09 01:10:28 +0900

    Change simple-hatena-root. ("~/.howm/hatena")

--- init-simple-hatena.el
+++ init-simple-hatena.el
@@ -1,24 +1,15 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
 
-;; based upon
 ;; http://coderepos.org/share/wiki/SimpleHatenaMode
-
 (when (require 'simple-hatena-mode nil t)
-  (setq simple-hatena-default-id "elim"
+  (setq simple-hatena-root "~/.howm/hatena"
+        simple-hatena-default-id "elim"
         simple-hatena-default-group "elim"
-        simple-hatena-use-timestamp-permalink-flag nil
+        simple-hatena-use-timestamp-permalink-flag t
         simple-hatena-time-offset 6
         simple-hatena-option-debug-flag t)
 
   (when (require 'hatenahelper-mode nil t)
     (add-hook 'simple-hatena-mode-hook
               '(lambda ()
-                 (hatenahelper-mode 1))))
-
-  (when (member '("utf-8-unix") coding-system-alist)
-    (mapc #'(lambda (arg)
-              (add-hook arg
-                        (lambda ()
-                          (setq buffer-file-coding-system 'utf-8-unix))))
-          (list 'howm-view-open-hook 'howm-create-file-hook))))
+                 (hatenahelper-mode 1))))))
