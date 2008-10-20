98692c2b6dd5e75d17de654adeb56866e4842e08
Takeru Naito <takeru.naito@gmail.com>
2008-10-20 16:16:39 +0900

    現在の css-mode 用に内容を変更。

--- init-css.el
+++ init-css.el
@@ -1,7 +1,4 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 (when (autoload-if-found 'css-mode "css-mode" nil t nil)
-  (setq auto-mode-alist
-        (cons '("\\.css\\'" . css-mode) auto-mode-alist)
-        cssm-indent-function #'cssm-c-style-indenter
-        cssm-indent-level 2))
+  (setq css-indent-offset 2))
