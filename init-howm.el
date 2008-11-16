eaa92675a2087fa0965fa33cb7975672880d56db
Takeru Naito <takeru.naito@gmail.com>
2008-11-17 05:29:08 +0900

    Fix Parenthese.

--- init-howm.el
+++ init-howm.el
@@ -8,7 +8,7 @@
         howm-directory (expand-file-name "~/.howm/")
         howm-keyword-file (expand-file-name "keys" howm-directory)
         howm-history-file (expand-file-name "history" howm-directory)
-        howm-view-use-grep (x->bool (executable-find "grep"))))
+        howm-view-use-grep (x->bool (executable-find "grep")))
 
   (mapc
    (lambda (f)
