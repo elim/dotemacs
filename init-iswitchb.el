7cc962cbd305a7959be3c8fe81dd18485645d876
Takeru Naito <takeru.naito@gmail.com>
2008-11-27 03:34:00 +0900

    anything がロードできないときだけ、 iswitchb を読み込む。

--- init-iswitchb.el
+++ init-iswitchb.el
@@ -1,26 +1,26 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-(unless (featurep 'anything)
-  (when (require 'iswitchb nil t)
-    (setq iswitchb-regexp t
-          iswitchb-buffer-ignore
-          (append
-           '("\\`[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\.howm\\'"
-             "\\.howm-keys"
-             "\\*WL.*"
-             "\\*Channel.*"
-             ".*skk.*"
-             "\\*\\(IMAP|NNTP|SMTP\\).*"
-             iswitchb-buffer-ignore)))
+(and (not (require 'anything nil t))
+     (require 'iswitchb nil t)
+     (setq iswitchb-regexp t
+           iswitchb-buffer-ignore
+           (append
+            '("\\`[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\.howm\\'"
+              "\\.howm-keys"
+              "\\*WL.*"
+              "\\*Channel.*"
+              ".*skk.*"
+              "\\*\\(IMAP|NNTP|SMTP\\).*"
+              iswitchb-buffer-ignore)))
 
-    (add-hook
-     'iswitchb-define-mode-map-hook
-     (lambda ()
-       (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
-       (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
-       (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
-       (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+     (add-hook
+      'iswitchb-define-mode-map-hook
+      (lambda ()
+        (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+        (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
 
-    (add-hook 'emacs-startup-hook
-              (lambda ()
-                (iswitchb-mode 1)))))
+     (add-hook 'emacs-startup-hook
+               (lambda ()
+                 (iswitchb-mode 1))))
