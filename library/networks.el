d38db9406dd2696d0d55e98168e16bbf2b84bafd
Takeru Naito <takeru.naito@gmail.com>
2008-12-11 23:23:09 +0900

    nc と ssh があるか一応確認するようにしたった。

--- library/networks.el
+++ library/networks.el
@@ -25,39 +25,42 @@
 ;; based upon
 ;; http://deisui.bug.org/%7Eueno/memo/emacs-ssh.html
 ;; http://triaez.kaisei.org/%7Ekaoru/ssh/emacs-ssh.html
-;; http://www.meadowy.org/~gotoh/lisp/relay.el
-(defun open-ssh-stream-internal (&rest plist)
-  (let ((name (or (plist-get plist :name) name))
-        (buffer (or (plist-get plist :buffer) buffer))
-        (host (or (plist-get plist :host) host))
-        (service (or (plist-get plist :service) service))
-        (process-connection-type nil))
-    (start-process-shell-command
-     name buffer
-     (or (plist-get plist :ssh) "ssh")
-     (or (plist-get plist :sshflags) "-Caxq")
-     (plist-get plist :relay)
-     (or (plist-get plist :command) "nc -w 60")
-     host (format "%s" service))))
+;; http://www.meadowy.org/%7Egotoh/lisp/relay.el
+(and (executable-find "ssh")
+     (executable-find "nc")
 
-(defun open-ssh-stream (name buffer host service)
-  (open-ssh-stream-internal :relay "elim.teroknor.org"
-                            :name name
-                            :buffer buffer
-                            :host host
-                            :service service))
+     (defun open-ssh-stream-internal (&rest plist)
+       (let ((name (or (plist-get plist :name) name))
+             (buffer (or (plist-get plist :buffer) buffer))
+             (host (or (plist-get plist :host) host))
+             (service (or (plist-get plist :service) service))
+             (process-connection-type nil))
+         (start-process-shell-command
+          name buffer
+          (or (plist-get plist :ssh) "ssh")
+          (or (plist-get plist :sshflags) "-Caxq")
+          (plist-get plist :relay)
+          (or (plist-get plist :command) "nc -w 60")
+          host (format "%s" service))))
 
-(defadvice open-network-stream (around relay (name buffer host service)
-                                       activate)
-  "Open network stream with relaying."
-  (let
-      ((short-system-name (car (split-string (system-name) "\\.")))
-       (hosts (list "localhost" "idea" "elim.teroknor.org"))
-       (services (list 25 143 2010 6667)))
+     (defun open-ssh-stream (name buffer host service)
+       (open-ssh-stream-internal :relay "elim.teroknor.org"
+                                 :name name
+                                 :buffer buffer
+                                 :host host
+                                 :service service))
 
-    (if (and (not (member short-system-name hosts))
-             (member host hosts)
-             (member service services))
-        (setq ad-return-value
-              (open-ssh-stream name buffer host service))
-      ad-do-it)))
+     (defadvice open-network-stream (around relay (name buffer host service)
+                                            activate)
+       "Open network stream with relaying."
+       (let
+           ((short-system-name (car (split-string (system-name) "\\.")))
+            (hosts (list "localhost" "idea" "elim.teroknor.org"))
+            (services (list 25 143 2010 6667)))
+
+         (if (and (not (member short-system-name hosts))
+                  (member host hosts)
+                  (member service services))
+             (setq ad-return-value
+                   (open-ssh-stream name buffer host service))
+           ad-do-it))))
