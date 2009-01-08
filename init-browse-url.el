94e4b893522255786e55f250b26eb0375669ff40
Takeru Naito <takeru.naito@gmail.com>
2009-01-08 16:01:52 +0900

    Sophisticated.

--- init-browse-url.el
+++ init-browse-url.el
@@ -8,8 +8,13 @@
         browse-url-browser-function 'browse-url-generic)
   (cond
    (windows-p
-    (setq browse-url-generic-program "cmd"
-          browse-url-generic-args '("/c" "start")))
+    (cond
+     ((executable-find "cygstart")
+      (setq browse-url-generic-program "cygstart"
+            browse-url-generic-args '("--open")))
+     ((executable-find "cmd")
+      (setq browse-url-generic-program "cmd"
+            browse-url-generic-args '("/c" "start")))))
 
    (darwin-p
     (setq browse-url-generic-program "open"))
@@ -21,15 +26,15 @@
                      nil (list "x-www-browser"
                                "firefox"))))
    ((functionp #'w3m-browse-url)
-    (setq browse-url-browser-function #'w3m-browse-url))))
+    (setq browse-url-browser-function #'w3m-browse-url)))
 
 
-;; http://cgi.netlaputa.ne.jp/~kose/diary/?200209b&to=200209125#200209125
-(defadvice thing-at-point-url-at-point
-  (after thing-at-point-url-at-point-after activate)
-  "http://ttp:// to http://"
-  (setq ad-return-value
-        (if (string-match "http://ttp:\\(.*\\)" ad-return-value)
-            (concat "http:" (substring ad-return-value
-                                       (match-beginning 1) (match-end 1)))
-          ad-return-value)))
+  ;; http://cgi.netlaputa.ne.jp/~kose/diary/?200209b&to=200209125#200209125
+  (defadvice thing-at-point-url-at-point
+    (after thing-at-point-url-at-point-after activate)
+    "http://ttp:// to http://"
+    (setq ad-return-value
+          (if (string-match "http://ttp:\\(.*\\)" ad-return-value)
+              (concat "http:" (substring ad-return-value
+                                         (match-beginning 1) (match-end 1)))
+            ad-return-value))))
