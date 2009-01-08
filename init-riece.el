82135cf9994b4a9cc0b9c69591484ce2d54a4fcd
Takeru Naito <takeru.naito@gmail.com>
2009-01-08 15:04:11 +0900

    and => when.

--- init-riece.el
+++ init-riece.el
@@ -1,115 +1,115 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-(and (autoload-if-found 'riece "riece" nil t)
-     (setq riece-server-alist
-           '(("default"
-              :host "localhost"
-              :service 6667
-              :coding utf-8))
-           riece-ndcc-server-address "localhost"
-           riece-channel-buffer-mode t
-           riece-user-list-buffer-mode t
-           riece-layout "middle-left"
+(when (autoload-if-found 'riece "riece" nil t)
+  (setq riece-server-alist
+        '(("default"
+           :host "localhost"
+           :service 6667
+           :coding utf-8))
+        riece-ndcc-server-address "localhost"
+        riece-channel-buffer-mode t
+        riece-user-list-buffer-mode t
+        riece-layout "middle-left"
 
-           riece-preference-directory
-           (expand-file-name "riece" base-directory)
+        riece-preference-directory
+        (expand-file-name "riece" base-directory)
 
-           riece-addon-directory
-           (expand-file-name "addons" riece-preference-directory)
+        riece-addon-directory
+        (expand-file-name "addons" riece-preference-directory)
 
-           riece-saved-variables-file
-           (expand-file-name "save" riece-preference-directory)
+        riece-saved-variables-file
+        (expand-file-name "save" riece-preference-directory)
 
-           riece-variables-file
-           (expand-file-name "init" riece-preference-directory)
+        riece-variables-file
+        (expand-file-name "init" riece-preference-directory)
 
-           riece-variables-files (list riece-variables-file
-                                       riece-saved-variables-file)
+        riece-variables-files (list riece-variables-file
+                                    riece-saved-variables-file)
 
-           riece-saved-forms '(riece-channel-buffer-mode
-                               riece-others-buffer-mode
-                               riece-user-list-buffer-mode
-                               riece-channel-list-buffer-mode
-                               riece-layout riece-addons)
+        riece-saved-forms '(riece-channel-buffer-mode
+                            riece-others-buffer-mode
+                            riece-user-list-buffer-mode
+                            riece-channel-list-buffer-mode
+                            riece-layout riece-addons)
 
-           riece-addons '(riece-alias
-                          riece-button
-                          riece-ctcp riece-guess riece-highlight
-                          riece-history riece-icon riece-keyword
-                          riece-menu riece-skk-kakutei riece-unread
-                          riece-url)
+        riece-addons '(riece-alias
+                       riece-button
+                       riece-ctcp riece-guess riece-highlight
+                       riece-history riece-icon riece-keyword
+                       riece-menu riece-skk-kakutei riece-unread
+                       riece-url)
 
-           riece-my-nicks (list
-                           "elim"         "Elim"
-                           "えむりん"     "エムリン"
-                           "えりむ"       "エリム"
-                           "えりめらんく" "エリメランク"
-                           "えろす"       "エロス"
-                           "えろむ"       "エロム"
-                           "えろりむ"     "エロリム"
-                           "ろりむ"       "ロリム"
-                           "えりも"       "エリモ"        "襟裳"
-                           "たける"       "タケル")
+        riece-my-nicks (list
+                        "elim"         "Elim"
+                        "えむりん"     "エムリン"
+                        "えりむ"       "エリム"
+                        "えりめらんく" "エリメランク"
+                        "えろす"       "エロス"
+                        "えろむ"       "エロム"
+                        "えろりむ"     "エロリム"
+                        "ろりむ"       "ロリム"
+                        "えりも"       "エリモ"        "襟裳"
+                        "たける"       "タケル")
 
-           riece-not-my-nicks (list
-                               "たけるんば" "タケルンバ")
+        riece-not-my-nicks (list
+                            "たけるんば" "タケルンバ")
 
-           riece-keywords `(,@riece-my-nicks
-                            ,@riece-not-my-nicks
-                            ("[Ee]macs" . 0)
-                            ("[Rr]uby"  . 0)
-                            ("[Zz]sh"   . 0))
+        riece-keywords `(,@riece-my-nicks
+                         ,@riece-not-my-nicks
+                         ("[Ee]macs" . 0)
+                         ("[Rr]uby"  . 0)
+                         ("[Zz]sh"   . 0))
 
-           riece-notify-sound-player
-           (fold-left (lambda (x y)
-                        (or x (executable-find y)))
-                      nil (list "mplayer" "aplay" "esdplay"))
+        riece-notify-sound-player
+        (fold-left (lambda (x y)
+                     (or x (executable-find y)))
+                   nil (list "mplayer" "aplay" "esdplay"))
 
-           riece-notify-sound
-           (fold-left (lambda (x y)
-                        (or x (and (file-readable-p y) y)))
-                      nil (list "~/sounds/notify.wav"
-                                "/System/Library/Sounds/Funk.aiff")))
+        riece-notify-sound
+        (fold-left (lambda (x y)
+                     (or x (and (file-readable-p y) y)))
+                   nil (list "~/sounds/notify.wav"
+                             "/System/Library/Sounds/Funk.aiff")))
 
-     (and riece-notify-sound
-          riece-notify-sound-player
+  (and riece-notify-sound
+       riece-notify-sound-player
 
-          (defun riece-notify-keyword-function (keyword)
-            (when (and (not (member keyword riece-not-my-nicks))
-                       (member keyword riece-my-nicks))
-              (start-process keyword "*Messages*"
-                             riece-notify-sound-player
-                             (expand-file-name riece-notify-sound))))
+       (defun riece-notify-keyword-function (keyword)
+         (when (and (not (member keyword riece-not-my-nicks))
+                    (member keyword riece-my-nicks))
+           (start-process keyword "*Messages*"
+                          riece-notify-sound-player
+                          (expand-file-name riece-notify-sound))))
 
-          (add-hook 'riece-notify-keyword-functions
-                    #'riece-notify-keyword-function))
+       (add-hook 'riece-notify-keyword-functions
+                 #'riece-notify-keyword-function))
 
-     (defvar riece-notify-nicks (list))
-     (setq riece-notify-nicks (list "elim"))
+  (defvar riece-notify-nicks (list))
+  (setq riece-notify-nicks (list "elim"))
 
-     (defadvice riece-keyword-message-filter
-       (around extended-riece-keyword-message-filter (message) activate)
+  (defadvice riece-keyword-message-filter
+    (around extended-riece-keyword-message-filter (message) activate)
 
-       (let ((speaker (aref (riece-message-speaker message) 0))
-             (message-text (riece-message-text message)))
+    (let ((speaker (aref (riece-message-speaker message) 0))
+          (message-text (riece-message-text message)))
 
-         (mapcar (lambda (nick)
-                   (when (string-match nick speaker)
-                     (put-text-property 0 (length speaker)
-                                        'riece-overlay-face
-                                        riece-keyword-face
-                                        speaker)))
-                 riece-notify-nicks))
-       ad-do-it)
+      (mapcar (lambda (nick)
+                (when (string-match nick speaker)
+                  (put-text-property 0 (length speaker)
+                                     'riece-overlay-face
+                                     riece-keyword-face
+                                     speaker)))
+              riece-notify-nicks))
+    ad-do-it)
 
-     (eval-after-load "riece"
-       (lambda ()
-         (ad-activate-regexp
-          "^extended-riece-keyword-message-filter$")
-         (ad-activate-regexp
-          "^riece-force-connect-default-server$")))
+  (eval-after-load "riece"
+    (lambda ()
+      (ad-activate-regexp
+       "^extended-riece-keyword-message-filter$")
+      (ad-activate-regexp
+       "^riece-force-connect-default-server$")))
 
-     (add-hook 'riece-startup-hook
-               '(lambda ()
-                  (define-key riece-command-mode-map [(control x) (n)]
-                    'riece-command-enter-message-as-notice))))
+  (add-hook 'riece-startup-hook
+            '(lambda ()
+               (define-key riece-command-mode-map [(control x) (n)]
+                 'riece-command-enter-message-as-notice))))
