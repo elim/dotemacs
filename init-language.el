7ba4c977a1fba69fe505fee410bf617589657bd8
Takeru Naito <takeru.naito@gmail.com>
2009-01-09 23:37:25 +0900

    deleted default-process-coding-system value.

--- init-language.el
+++ init-language.el
@@ -14,9 +14,7 @@
      (setenv "LC_CTYPE"
              (or (getenv "LC_CTYPE") "ja_JP.UTF-8"))
      (set-language-environment 'Japanese)
-     (setq default-process-coding-system
-           '(utf-8-unix . utf-8-unix))
-
+   
      ;; modified coding detection priority. (low => high)
      (mapc  #'prefer-coding-system
             '(shift_jis iso-2022-jp euc-jp utf-8))
