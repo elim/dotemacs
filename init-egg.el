cd2a5698352f8afb45b0e1591c8a271a08052d3f
Takeru Naito <takeru.naito@gmail.com>
2009-01-09 23:29:10 +0900

    Added hook for revert file mode (if needed).

--- init-egg.el
+++ init-egg.el
@@ -2,7 +2,31 @@
 
 (when (require 'egg nil t)
   (load "egg-grep" t)
-  (add-to-list 'process-coding-system-alist '("git"      . utf-8))
+  (add-to-list 'process-coding-system-alist '("git" . utf-8))
   (add-hook 'dired-mode-hook
-            '(lambda ()
-               (define-key dired-mode-map "V" 'egg-status))))
+            (lambda ()
+              (define-key dired-mode-map "V" 'egg-status)))
+  
+
+(and nt-p
+     (executable-find "chmod")
+     (add-hook 'after-save-hook
+               (lambda ()
+                 (let*
+                     ((git-dir (egg-git-dir))
+                      (base-dir (and git-dir
+                                     (string-match "\\(.+\\).git" git-dir)
+                                     (match-string 1 git-dir)))
+                      (file-name (and base-dir
+                                      (string-match (format "%s\\(.+\\)" base-dir)
+                                                    (buffer-file-name))
+                                      (match-string 1 (buffer-file-name))))
+                      (old-mode (and file-name
+                                     (with-temp-buffer
+                                       (call-process "git" nil t t "diff" file-name)
+                                       (goto-char (point-min))
+                                       (when (re-search-forward "^old mode.+\\([0-9]..\\)$" nil t)
+                                         (match-string 1))))))
+
+                   (when old-mode
+                     (call-process "chmod" nil t t old-mode (buffer-file-name))))))))
