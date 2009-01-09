d9e697e27d76f518c4b536346eb1010f7efdb4bb
Takeru Naito <takeru.naito@gmail.com>
2009-01-09 23:34:05 +0900

    Added advice force-r/w-utf8.

--- init-simple-hatena.el
+++ init-simple-hatena.el
@@ -14,15 +14,26 @@
          `((,simple-hatena-filename-regex utf-8-unix . utf-8-unix))
          file-coding-system-alist))
 
-  '(add-hook 'simple-hatena-before-submit-hook
+  (add-hook 'simple-hatena-before-submit-hook
             '(lambda ()
                (setq buffer-file-coding-system 'utf-8-unix)))
-
+  
   (defadvice simple-hatena-internal-safe-find-file (after
                                                     insert-fixed-expression
                                                     activate)
     (unless (file-exists-p (buffer-file-name))
       (insert "delete\n*")))
+
+  (defadvice simple-hatena-internal-safe-find-file (around
+                                                    force-r/w-utf8 (filename)
+                                                    activate)
+    (let*
+        ((coding-system 'utf-8-unix)
+         (coding-system-for-read coding-system)
+         (coding-system-for-write coding-system)
+         (coding-system-require-warning t))
+      ad-do-it))
+          
 
   (let
       ((helper (or (require 'simple-hatenahelper-mode nil t)
