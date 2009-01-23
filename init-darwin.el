f510e56b87e6efeb42d62d00fd557e3cd2aaaa10
Takeru Naito <takeru.naito@gmail.com>
2009-01-23 14:31:41 +0900

    Cocoa Emacs 対策。

--- init-darwin.el
+++ init-darwin.el
@@ -1,7 +1,9 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-;; Carbon Emacs
-(when carbon-p
+(cond
+ (carbon-p
   (setq mac-pass-control-to-system nil
         mac-pass-command-to-system nil
         mac-option-modifier 'meta))
+ (ns-p
+  (setq ns-command-modifier 'meta)))
