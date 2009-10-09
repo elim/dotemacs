7c18fb1910734ca9e358257b97759dc9041d7bb5
Takeru Naito <takeru.naito@gmail.com>
2009-10-09 11:01:36 +0900

    set [ns-drag-file] to find-file.

--- init-darwin.el
+++ init-darwin.el
@@ -1,9 +1,15 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-(cond
- (carbon-p
-  (setq mac-pass-control-to-system nil
-        mac-pass-command-to-system nil
-        mac-option-modifier 'meta))
- (ns-p
-  (setq ns-command-modifier 'meta)))
+(and
+ darwin-p window-system
+ (setq mac-allow-anti-aliasing nil
+       mac-pass-control-to-system nil
+       mac-pass-command-to-system nil
+       mac-pass-option-to-system nil
+       mac-option-modifier nil
+       mac-function-modifier nil
+       mac-option-modifier 'meta
+
+       ns-antialias-text nil
+       ns-command-modifier 'meta)
+ (define-key global-map [ns-drag-file] 'ns-find-file))
