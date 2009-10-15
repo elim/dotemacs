5dc90f240b18d9df4f77c1fada792d768dbeca7e
Takeru Naito <takeru.naito@gmail.com>
2009-10-16 00:56:54 +0900

    Sophisticated.

--- init-session.el
+++ init-session.el
@@ -2,12 +2,13 @@
 
 ;; http://d.hatena.ne.jp/higepon/20061230/1167447339
 (when (require 'session nil t)
-  (setq history-length t)
-  (setq session-initialize '(de-saveplace session keys menus places))
-  (setq session-globals-include '((kill-ring 255)
+  (setq history-length t
+        session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 255)
                                   (session-file-alist 255 t)
-                                  (file-name-history 255)))
-  (setq session-globals-max-size 1024)
-  (setq session-globals-max-string 1024)
-  (setq session-save-file (expand-file-name "session-save.el" user-emacs-directory))
+                                  (file-name-history 255))
+        session-globals-max-size 1024
+        session-globals-max-string 1024
+        session-save-file (expand-file-name
+                           "session-save.el" user-emacs-directory))
   (add-hook 'after-init-hook 'session-initialize))
