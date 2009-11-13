cd1f155019fc1a4d51b149ed15f294cc7e33cb1d
Takeru Naito <takeru.naito@gmail.com>
2009-11-13 13:33:51 +0900

    Sophisticated.

--- init-html-helper.el
+++ init-html-helper.el
@@ -1,11 +1,12 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 (when (autoload-if-found 'html-helper-mode "html-helper-mode" "Yay HTML" t)
-  (setq html-helper-new-buffer-template nil)
-  (setq html-helper-do-write-file-hooks t)
-  (setq html-helper-build-new-buffer nil)
-  (setq html-helper-basic-offset 1)
-  (setq html-helper-item-continue-indent 2)
-  (setq html-helper-address-string "elim@teroknor.org")
-  (setq auto-mode-alist
-        (cons '("\\.html$" . html-helper-mode) auto-mode-alist)))
+  (setq html-helper-new-buffer-template nil
+        html-helper-do-write-file-hooks t
+        html-helper-build-new-buffer nil
+        html-helper-basic-offset 1
+        html-helper-item-continue-indent 2
+        html-helper-address-string user-mail-address
+        html-helper-verbose nil
+  auto-mode-alist
+   (cons '("\\.html$" . html-helper-mode) auto-mode-alist)))
