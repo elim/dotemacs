10654d9495cde67a2630e70590569847fea17588
Takeru Naito <takeru.naito@gmail.com>
2010-04-10 13:38:50 +0900

    auto-install の呼び方を変えた。あと auto-install はリポジトリに含める事にした。無いと暮らせないし。

--- init.el
+++ init.el
@@ -118,17 +118,11 @@
               (message "`%s' loaded." file))) files)))
 
 ;; load auto-install.el.
+(setq auto-install-directory
+      (concat user-emacs-directory "auto-install/"))
+(add-to-list 'load-path auto-install-directory)
 
 (when (require 'auto-install nil t)
-  (setq auto-install-directory
-        (expand-file-name (concat
-                           user-emacs-directory
-                           "auto-install"))
-        load-path
-        (merge-path-list
-         load-path
-         (list auto-install-directory)))
-
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
 
