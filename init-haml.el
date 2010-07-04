f36ab7598c8a3cb6d8560bc5e9b82a3c49958dc2
Takeru Naito <takeru.naito@gmail.com>
2010-07-05 00:01:14 +0900

    new: init-haml.el and init-sass.el.

---
+++ init-haml.el
@@ -0,0 +1,4 @@
+;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
+;; (auto-install-from-url "http://github.com/nex3/haml/raw/master/extra/haml-mode.el")
+
+(require 'haml-mode nil t)
