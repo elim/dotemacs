9ea73d9a396d982e0b55f1f3f22eb61e71c64478
Takeru Naito <takeru.naito@gmail.com>
2010-08-08 22:45:17 +0900

    add: Cocoa Emacs pasteboard issue.

--- init-darwin.el
+++ init-darwin.el
@@ -1,7 +1,10 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 (and
- darwin-p window-system
+ darwin-p
+ (or
+  (eq window-system 'mac)
+  (eq window-system 'ns))
  (setq mac-allow-anti-aliasing nil
        mac-pass-control-to-system nil
        mac-pass-command-to-system nil
@@ -11,5 +14,9 @@
        mac-option-modifier 'meta
 
        ns-antialias-text nil
-       ns-command-modifier 'meta)
+       ns-command-modifier 'meta
+       x-select-enable-clipboard nil
+       x-select-enable-primary t
+       select-active-regions nil)
+
  (define-key global-map [ns-drag-file] 'ns-find-file))
