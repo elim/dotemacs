cb630978dc88f7cee7d9d00f9cebfcdbb4deec02
Takeru Naito <takeru.naito@gmail.com>
2012-05-22 20:14:18 +0900

    init-server.el: remove-hook server-kill-buffer-query-function. Stage this hunk [y,n,q,a,d,/,e,?]? y

--- init-server.el
+++ init-server.el
@@ -2,4 +2,7 @@
 
 (when (functionp 'server-start)
   (setq server-window 'pop-to-buffer)
-  (server-start))
+  (server-start)
+  (remove-hook
+   'kill-buffer-query-functions
+   'server-kill-buffer-query-function))
