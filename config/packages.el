6fb8bf247ecbd9d2107ab07f1278142faa9eabd3
Takeru Naito <takeru.naito@gmail.com>
2012-08-29 23:40:17 +0900

    Modify setting for popwin

--- config/packages.el
+++ config/packages.el
@@ -89,12 +89,7 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer
       anything-samewindow nil)
-(push '("*anything*" :height 20) popwin:special-display-config)
-(push '("*anything M-x*" :height 20) popwin:special-display-config)
-(push '("*anything for files*" :height 20) popwin:special-display-config)
-(push '("*anything find-file*" :height 20) popwin:special-display-config)
-(push '(dired-mode :position top) popwin:special-display-config)
-
+(push '("\*anything.+" :height 20 :regexp t) popwin:special-display-config)
 
 ;;; markdown-mode
 ;;
