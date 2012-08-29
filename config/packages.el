4b9ee472c28633044cb64aa7d70e639704d10e1f
Takeru Naito <takeru.naito@gmail.com>
2012-08-29 23:42:22 +0900

    Setup mew

--- config/packages.el
+++ config/packages.el
@@ -114,6 +114,28 @@
 (define-key global-map (kbd "C-x C-z") 'open-junk-file)
 
 
+;;; Mew
+;;
+(el-get 'sync '(mew))
+
+;; Optional setup (Read Mail menu):
+(setq read-mail-command 'mew
+      mew-debug t
+      mew-rc-file (expand-file-name "~/.emacs.d/config/packages/mew.el"))
+
+;; Optional setup (e.g. C-xm for sending a message):
+(autoload 'mew-user-agent-compose "mew" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'mew-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'mew-user-agent
+      'mew-user-agent-compose
+      'mew-draft-send-message
+      'mew-draft-kill
+      'mew-send-hook))
+
+
 ;;; twittering-mode
 ;;
 (el-get 'sync '(twittering-mode))
