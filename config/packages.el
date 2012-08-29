a5688c7640e8abc96065f53d6370ade0f68d399d
Takeru Naito <takeru.naito@gmail.com>
2012-08-29 23:29:28 +0900

    Sophisticate ruby-mode indentation

--- config/packages.el
+++ config/packages.el
@@ -219,6 +219,23 @@
 (el-get 'sync '(ruby-mode-trunk))
 (el-get 'sync '(ruby-end))
 
+;; http://stackoverflow.com/questions/7961533/emacs-ruby-method-parameter-indentation
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+
 
 ;;; rabbit-mode
 ;; 2012-03-16
