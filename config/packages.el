8d7787b5e1fed6843937a103376406f28d4f6390
Takeru Naito <takeru.naito@gmail.com>
2012-09-28 18:00:39 +0900

    Use a session package of el-get

--- config/packages.el
+++ config/packages.el
@@ -18,6 +18,24 @@
              "~/.emacs.d/config/el-get/local-recipes")
 
 
+;;; session
+;;
+; (el-get 'sync '(session))
+(el-get 'sync '(emacs-goodies-el))
+(when (require 'session nil t)
+  (setq history-length t
+        session-save-file-coding-system 'utf-8-unix
+        session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 1024)
+                                  (session-file-alist 1024 t)
+                                  (file-name-history 1024))
+        session-globals-max-size 1024
+        session-globals-max-string 1024
+        session-save-file (expand-file-name
+                           "session-save.el" user-emacs-directory))
+  (add-hook 'after-init-hook 'session-initialize))
+
+
 ;;; lispxmp
 ;; 式の評価結果を注釈するための設定
 (el-get 'sync '(lispxmp))
