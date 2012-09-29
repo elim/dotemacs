703b52e84c354aaa91f5b41bdbbaf9a943acb846
Takeru Naito <takeru.naito@gmail.com>
2012-09-29 12:29:10 +0900

    Makeshift solution for '(void-variable c-font-lock-keywords-3)'

--- config/builtins/cc-mode.el
+++ config/builtins/cc-mode.el
@@ -3,25 +3,26 @@
 ;; 2012-03-18
 
 ;; c-modeやc++-modeなどcc-modeベースのモード共通の設定
-(add-hook
- 'c-mode-common-hook
- (lambda ()
-   ;; BSDスタイルをベースにする
-   (c-set-style "bsd")
+(when (require 'cc-mode nil t)
+  (add-hook
+   'c-mode-common-hook
+   (lambda ()
+     ;; BSDスタイルをベースにする
+     (c-set-style "bsd")
 
-   ;; スペースでインデントをする
-   (setq indent-tabs-mode nil)
+     ;; スペースでインデントをする
+     (setq indent-tabs-mode nil)
 
-   ;; インデント幅を2にする
-   (setq c-basic-offset 2)
+     ;; インデント幅を2にする
+     (setq c-basic-offset 2)
 
-   ;; 自動改行（auto-new-line）と
-   ;; 連続する空白の一括削除（hungry-delete）を
-   ;; 有効にする
-   (c-toggle-auto-hungry-state -1)
+     ;; 自動改行（auto-new-line）と
+     ;; 連続する空白の一括削除（hungry-delete）を
+     ;; 有効にする
+     (c-toggle-auto-hungry-state -1)
 
-   ;; CamelCaseの語でも単語単位に分解して編集する
-   ;; GtkWindow         => Gtk Window
-   ;; EmacsFrameClass   => Emacs Frame Class
-   ;; NSGraphicsContext => NS Graphics Context
-   (subword-mode 1)))
+     ;; CamelCaseの語でも単語単位に分解して編集する
+     ;; GtkWindow         => Gtk Window
+     ;; EmacsFrameClass   => Emacs Frame Class
+     ;; NSGraphicsContext => NS Graphics Context
+     (subword-mode 1))))
