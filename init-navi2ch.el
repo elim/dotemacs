38034478d101ac78814bc4f0f5a334f0b0636531
Takeru Naito <takeru.naito@gmail.com>
2012-11-06 19:20:57 +0900

    Remove a no longer used navi2ch

--- init-navi2ch.el
+++
@@ -1,7 +0,0 @@
-;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-
-(when (autoload
-       'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
-  (setq navi2ch-mona-enable t)
-  (setq navi2ch-list-bbstable-url
-        "http://azlucky.s25.xrea.com/2chboard/bbsmenu2.html"))
