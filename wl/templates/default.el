b4e5dc3889de83a02f5af908c627f3fa9da36cfd
Takeru Naito <takeru.naito@gmail.com>
2012-11-06 19:23:23 +0900

    Remove a no longer used wanderlust

--- wl/templates/default.el
+++
@@ -1,7 +0,0 @@
-;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-
-(add-to-list
- 'wl-template-alist
- `(,(file-name-sans-extension
-    (file-name-nondirectory load-file-name))
-   ("Organization" . nil)))
--- wl/templates/teroknor.el
+++
@@ -1,9 +0,0 @@
-;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
-;;; $Id$
-
-(add-to-list
- 'wl-template-alist
- `(,(file-name-sans-extension
-     (file-name-nondirectory load-file-name))
-   (wl-from . "elim garak <elim@teroknor.org>")
-   ("From" . wl-from)))
