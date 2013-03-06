8410bbc8942f64641224645f4b30ecf3fef16d35
Takeru Naito <takeru.naito@gmail.com>
2013-03-06 10:53:56 +0900

    Disable popup

--- config/packages.el
+++ config/packages.el
@@ -425,11 +425,6 @@
             (define-key grep-mode-map
               (kbd "C-c C-c") 'grep-edit-finish-edit)))
 
-
-;;; ポップアップ
-;; 2012-03-16
-(el-get 'sync '(popup))
-
 
 ;;; Auto Complete
 ;; 自動補完
