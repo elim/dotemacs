627ad60d091ab33a77cdc8dd7a393c5ae8536056
Takeru Naito <takeru.naito@gmail.com>
2015-12-04 11:48:51 +0900

    Reset the settings of the web-mode
    - Separate the loading and the configurations

--- config/packages.el
+++ config/packages.el
@@ -419,41 +419,13 @@
   :prepare (autoload 'tiarra-conf-mode "tiarra-conf"
              "Major mode for editing Tiarra configuration file." t))
 
-(el-get-bundle web-mode
-  (with-eval-after-load-feature 'web-mode
-    (setq auto-mode-alist
-          (append
-           '(("/\\(PEAR\\|pear\\)/" . php-mode)
-             ("\.html?$" . web-mode)
-             ("\.ctp$" . web-mode))
-           auto-mode-alist)
-          web-mode-block-padding 2
-          web-mode-comment-style 2
-          web-mode-indent-style 2
-          web-mode-script-padding 2
-          web-mode-style-padding 2)
-
-    (add-hook 'web-mode-hook
-              #'(lambda ()
-                  (setq tab-width 2
-                        indent-tabs-mode nil
-                        show-trailing-whitespace t
-                        require-final-newline t
-                        c-basic-offset 2
-                        ;; コメントのスタイル (必要なければコメントアウトする)
-                        comment-start "// "
-                        comment-end   ""
-                        comment-start-skip "// *")
-                  (hs-minor-mode 1)
-                  (with-eval-after-load-feature 'dabbrev
-                    (set (make-local-variable 'dabbrev-abbrev-skip-leading-regexp) "$"))))))
-
 (el-get-bundle wgrep
   :features (wgrep wgrep-ag))
 
 
 (el-get-bundle twittering-mode)
 (el-get-bundle visual-regexp)
+(el-get-bundle web-mode)
 (el-get-bundle yaml-mode)
 
 ;;; packages.el ends here
---
+++ config/packages/init-web-mode.el
@@ -0,0 +1,33 @@
+;;; init-web-mode.el --- A setting of the web-mode.
+;;; Commentary:
+;;; Code:
+
+(setq auto-mode-alist
+      (append
+       '(("/\\(PEAR\\|pear\\)/" . php-mode)
+         ("\.html?$" . web-mode)
+         ("\.ctp$" . web-mode))
+       auto-mode-alist)
+      web-mode-block-padding 2
+      web-mode-comment-style 2
+      web-mode-indent-style 2
+      web-mode-script-padding 2
+      web-mode-style-padding 2)
+
+(add-hook 'web-mode-hook
+          #'(lambda ()
+              (setq tab-width 2
+                    indent-tabs-mode nil
+                    show-trailing-whitespace t
+                    require-final-newline t
+                    c-basic-offset 2
+                    ;; コメントのスタイル (必要なければコメントアウトする)
+                    comment-start "// "
+                    comment-end   ""
+                    comment-start-skip "// *")
+              (hs-minor-mode 1)
+              (with-eval-after-load-feature 'dabbrev
+                (set (make-local-variable 'dabbrev-abbrev-skip-leading-regexp) "$"))))
+
+(provide 'init-web-mode)
+;;; init-web-mode.el ends here
