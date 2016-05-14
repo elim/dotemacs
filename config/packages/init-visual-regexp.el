9f6577011f47074097c03bb241658a8e0bbc597d
Takeru Naito <takeru.naito@gmail.com>
2016-05-14 15:18:41 +0900

    Hello anzu, Goodbye visual-regexp

--- config/packages/init-visual-regexp.el
+++
@@ -1,9 +0,0 @@
-;;; init-visual-regexp.el --- A setting of the visual-regexp.
-;;; Commentary:
-;;; Code:
-
-(global-set-key (kbd "M-%")   'vr/query-replace)
-(global-set-key (kbd "C-M-%") 'vr/replace)
-
-(provide 'init-visual-regexp)
-;;; init-visual-regexp.el ends here
