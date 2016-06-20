5ff5447243e591085da4b4970861fdabe001cff6
Takeru Naito <takeru.naito@gmail.com>
2016-06-20 12:03:25 +0900

    Update setting of the anzu
    Referring to the recommend settings,
    http://qiita.com/syohex/items/56cf3b7f7d9943f7a7ba

--- config/packages/init-anzu.el
+++ config/packages/init-anzu.el
@@ -3,6 +3,11 @@
 ;;; Code:
 
 (with-eval-after-load-feature 'anzu
+  (setq anzu-use-migemo t
+        anzu-mode-lighter ""
+        anzu-deactivate-region t
+        anzu-search-threshold 1000)
+
   (global-anzu-mode +1)
   (global-set-key [remap query-replace] 'anzu-query-replace)
   (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp))
