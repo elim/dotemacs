d0b75f0bfe53d83f76c8d3caa984c9b8bcd0213d
Takeru Naito <takeru.naito@gmail.com>
2016-10-03 10:24:00 +0900

    Cleanup the web-mode settings

--- config/packages/init-web-mode.el
+++ config/packages/init-web-mode.el
@@ -2,30 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq auto-mode-alist
-      (append
-       '(("/\\(PEAR\\|pear\\)/" . php-mode)
-         ("\.html?$" . web-mode)
-         ("\.ctp$" . web-mode))
-       auto-mode-alist)
-      web-mode-block-padding 2
-      web-mode-comment-style 2
-      web-mode-indent-style 2
-      web-mode-script-padding 2
-      web-mode-style-padding 2)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 
-(add-hook 'web-mode-hook
-          #'(lambda ()
-              (setq tab-width 2
-                    indent-tabs-mode nil
-                    show-trailing-whitespace t
-                    require-final-newline t
-                    c-basic-offset 2
-                    ;; コメントのスタイル (必要なければコメントアウトする)
-                    comment-start "// "
-                    comment-end   ""
-                    comment-start-skip "// *")
-              (hs-minor-mode 1)))
+(setq web-mode-block-padding 2)
+(setq web-mode-comment-style 2)
+(setq web-mode-indent-style 2)
+(setq web-mode-script-padding 2)
+(setq web-mode-style-padding 2)
 
 (provide 'init-web-mode)
+
 ;;; init-web-mode.el ends here
