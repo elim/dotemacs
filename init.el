8b67d20dc536e4f53ea0f7eebd14a15827d6ec90
Takeru Naito <takeru.naito@gmail.com>
2016-10-05 13:41:45 +0900

    Remove the system-predicate for the GNU/Linux

--- init.el
+++ init.el
@@ -40,7 +40,6 @@
 ;; system-type predicates
 (setq darwin-p  (eq system-type 'darwin)
       ns-p      (featurep 'ns)
-      linux-p   (eq system-type 'gnu/linux)
       nt-p      (eq system-type 'windows-nt))
 
 (setq custom-file (expand-file-name "customize.el" user-emacs-directory))
