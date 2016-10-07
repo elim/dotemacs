fff89f742ad0e7cf630a89143a51e5d3bb52d206
Takeru Naito <takeru.naito@gmail.com>
2016-10-07 12:11:39 +0900

    Good bye Mew, You were really pretty cat

--- config/packages.el
+++ config/packages.el
@@ -105,28 +105,6 @@
     (define-key markdown-mode-map (kbd "C-c 4")   'markdown-insert-header-atx-4)
     (define-key markdown-mode-map (kbd "M-RET")   'markdown-insert-list-item)))
 
-;;; Mew
-;;
-(el-get-bundle mew
-  ;; Optional setup (Read Mail menu):
-
-  (with-eval-after-load-feature 'mew
-    (setq read-mail-command 'mew
-          mew-debug t
-          mew-rc-file (expand-file-name "config/packages/mew.el" user-emacs-directory)))
-
-  ;; Optional setup (e.g. C-xm for sending a message):
-  (autoload 'mew-user-agent-compose "mew" nil t)
-  (if (boundp 'mail-user-agent)
-      (setq mail-user-agent 'mew-user-agent))
-  (if (fboundp 'define-mail-user-agent)
-      (define-mail-user-agent
-        'mew-user-agent
-        'mew-user-agent-compose
-        'mew-draft-send-message
-        'mew-draft-kill
-        'mew-send-hook)))
-
 (el-get-bundle hg:tiarra-conf-mode
   :description "Emacs mode for editing Tiarra configuration."
   :url "https://bitbucket.org/topia/tiarra"
