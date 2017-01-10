9727de7de7e4374f7234a7679d68d16e0c1784e8
Takeru Naito <takeru.naito@gmail.com>
2017-01-10 14:26:49 +0900

    Apply valid styles to the environment.el

--- config/environment.el
+++ config/environment.el
@@ -1,14 +1,13 @@
-;;; 
-;;
+;;; environment.el -- A setting of the environments.
+;;; Commentary:
+;;; Code:
+
 (setq select-enable-clipboard t)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;;; 
-;;
 (load "environment/frame")
-
-
-;;; 
-;;
 (load "environment/fonts")
+
+;;; environment.el ends here
+
