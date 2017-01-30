d8626fec5223ed543de1a68f913940ac2aa595b0
Takeru Naito <takeru.naito@gmail.com>
2017-01-30 09:38:37 +0900

    Can toggle the clipboard observation

--- config/packages/init-clipboard-to-kill-ring.el
+++ config/packages/init-clipboard-to-kill-ring.el
@@ -6,6 +6,15 @@
 
 (require 'clipboard-to-kill-ring)
 
+(defun elim:toggle-clipboard-to-kill-ring ()
+  "Toggle `clipboard-to-kill-ring' activity"
+  (interactive)
+  (let ((desired-state (not clipboard-to-kill-ring:timer)))
+    (clipboard-to-kill-ring desired-state)
+    (message "clipboard-to-kill-ring %s" (if desired-state "on" "off"))))
+
+(define-key ctl-x-map (kbd "ck") 'elim:toggle-clipboard-to-kill-ring)
+
 (clipboard-to-kill-ring +1)
 
 (provide 'init-clipboard-to-kill-ring)
