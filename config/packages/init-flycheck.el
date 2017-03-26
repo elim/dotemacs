369df55e2cb54e1db3985c616513d78ccbfe198b
Takeru Naito <takeru.naito@gmail.com>
2017-03-26 22:24:46 +0900

    Prepend "bundle exec" when it's bundled executable

--- config/packages/init-flycheck.el
+++ config/packages/init-flycheck.el
@@ -4,6 +4,21 @@
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq flycheck-checker-error-threshold 5000)
+(setq flycheck-command-wrapper-function #'elim:flycheck-command-wrapper)
+
+(defun elim:flycheck-command-wrapper (command-list)
+  (let
+    ((bundled-executables (list "rubocop" "slim-lint" "scss-lint"))
+      (executable (car command-list))
+      (arguments (cdr command-list))
+      (replaced-command-list))
+
+    (dolist (bundled-executable bundled-executables)
+      (when (string-match bundled-executable executable)
+        (setq replaced-command-list
+          (append (list "bundle" "exec" bundled-executable) arguments))))
+
+    (or replaced-command-list command-list)))
 
 (provide 'init-flycheck)
 
