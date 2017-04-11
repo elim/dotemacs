3396f1f8e054f665f1780b9f58153be78eac7385
Takeru Naito <takeru.naito@gmail.com>
2017-04-12 08:42:31 +0900

    Use the jinja2-mode in any jinja2 files

---
+++ config/packages/init-jinja2-mode.el
@@ -0,0 +1,9 @@
+;;; init-jinja2-mode.el --- A setting of the jinja2-mode.
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.jinja\\'"  . jinja2-mode))
+
+(provide 'init-jinja2-mode)
+
+;;; init-jinja2-mode.el ends here
