6a24ac397ba214e545b35873e2301978ea4493d2
Takeru Naito <takeru.naito@gmail.com>
2017-04-12 09:11:43 +0900

    Use the salt-mode instead of the customized yaml-mode in SaltStack

--- config/packages/init-yaml-mode.el
+++ config/packages/init-yaml-mode.el
@@ -2,16 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(define-derived-mode saltstack-mode yaml-mode "Saltstack"
-  "Minimal Saltstack mode, based on `yaml-mode'."
-  (setq tab-width 2
-        indent-tabs-mode nil))
-
-(add-to-list 'auto-mode-alist '("\\.sls\\'"     . saltstack-mode))
-(add-to-list 'auto-mode-alist '("\\master\\'"   . saltstack-mode))
-(add-to-list 'auto-mode-alist '("\\roster\\'"   . saltstack-mode))
-(add-to-list 'auto-mode-alist '("\\Saltfile\\'" . saltstack-mode))
-
 (provide 'init-yaml-mode)
 
 ;;; init-yaml-mode.el ends here
