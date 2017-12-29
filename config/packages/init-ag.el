18a2ce5b80a3ddb23d5b48d97249c485bd086ec7
Takeru Naito <takeru.naito@gmail.com>
2017-12-29 17:27:27 +0900

    Apply the use-package format to the ag configurations

--- config/packages/init-ag.el
+++ config/packages/init-ag.el
@@ -2,41 +2,39 @@
 ;;; Commentary:
 ;;; Code:
 
-(with-eval-after-load-feature 'ag
-  (add-to-list 'process-coding-system-alist '("ag" utf-8 . utf-8))
+(defun elim:filter (condp lst)
+  (delq nil
+        (mapcar #'(lambda (x) (and (funcall condp x) x)) lst)))
 
-  (setq ag-highlight-search t  ; 検索結果の中の検索語をハイライトする
-        ag-reuse-window nil    ; 現在のウィンドウを検索結果表示に使う
-        ag-reuse-buffers 'nil) ; 現在のバッファを検索結果表示に使う
-
-  (autoload 'wgrep-ag-setup "wgrep-ag")
-  (add-hook 'ag-mode-hook 'wgrep-ag-setup)
-
-  ;; agの検索結果バッファで"r"で編集モードに。
-  ;; C-x C-sで保存して終了、C-x C-kで保存せずに終了
-  (define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
-
-  ;; キーバインドを適当につけておくと便利。"\C-xg"とか
-  (global-set-key [(super m)] 'ag)
-
-  ;; ag開いたらagのバッファに移動するには以下をつかう
-  (defun my/filter (condp lst)
-    (delq nil
-          (mapcar #'(lambda (x) (and (funcall condp x) x)) lst)))
-
-  (defun my/get-buffer-window-list-regexp (regexp)
-    "Return list of windows whose buffer name matches regexp."
-    (my/filter #'(lambda (window)
+(defun elim:get-buffer-window-list-regexp (regexp)
+  "Return list of windows whose buffer name match REGEXP."
+  (elim:filter #'(lambda (window)
                    (string-match regexp
                                  (buffer-name (window-buffer window))))
                (window-list)))
 
-  (global-set-key [(super m)]
-                  #'(lambda ()
-                      (interactive)
-                      (call-interactively 'ag)
-                      (select-window ; select ag buffer
-                       (car (my/get-buffer-window-list-regexp "^\\*ag "))))))
+(use-package ag
+  :bind (("C-x g" . elim:ag) ;; キーバインドを適当につけておくと便利。"\C-xg"とか
+         :map ag-mode-map
+         ;; agの検索結果バッファで"r"で編集モードに。
+         ;; C-x C-sで保存して終了、C-x C-kで保存せずに終了
+         ("r" . wgrep-change-to-wgrep-mode))
+  :commands wgrep-ag-setup
+
+  :hook (ag . wgrep-ag-setup)
+
+  :init
+  (add-to-list 'process-coding-system-alist '("ag" utf-8 . utf-8))
+  (set-variable 'ag-highlight-search t) ; 検索結果の中の検索語をハイライトする
+  (set-variable 'ag-reuse-buffers nil)  ; 現在のバッファを検索結果表示に使う
+  (set-variable 'ag-reuse-window nil)   ; 現在のウィンドウを検索結果表示に使う
+
+  (defun elim:ag ()
+    "Open and select the ag result buffer."
+    (interactive)
+    (call-interactively 'ag)
+    (select-window ; select ag buffer
+     (car (elim:get-buffer-window-list-regexp "^\\*ag ")))))
 
 (provide 'init-ag)
 ;;; init-ag.el ends here
