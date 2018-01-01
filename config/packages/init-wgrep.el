9e3adc5fe87f53dd567d805644a4877aadf413b7
Takeru Naito <takeru.naito@gmail.com>
2018-01-01 21:50:22 +0900

    Automatically saving when finish ediging with the wgrep

---
+++ config/packages/init-wgrep.el
@@ -0,0 +1,10 @@
+;;; init-wgrep.el --- A setting of the wgrep.
+;;; Commentary:
+;;; Code:
+
+(use-package wgrep
+  :config
+  (set-variable 'wgrep-auto-save-buffer t))
+
+(provide 'init-wgrep)
+;;; init-wgrep.el ends here
