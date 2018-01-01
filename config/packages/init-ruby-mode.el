412bea99fee134064a0457b606df37fde5c761ba
Takeru Naito <takeru.naito@gmail.com>
2018-01-02 01:41:43 +0900

    Apply the use-package format to the ruby-mode configurations

--- config/packages/init-ruby-mode.el
+++ config/packages/init-ruby-mode.el
@@ -2,10 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq ruby-deep-indent-paren-style nil
-      ruby-insert-encoding-magic-comment nil)
-
-(define-key ruby-mode-map "\C-m" 'reindent-then-newline-and-indent)
+(use-package ruby-mode
+  :bind  (:map ruby-mode-map
+               ("C-m" . reindent-then-newline-and-indent))
+  :init
+  (set-variable 'ruby-deep-indent-paren-style nil)
+  (set-variable 'ruby-insert-encoding-magic-comment nil))
 
 (provide 'init-ruby-mode)
 ;;; init-ruby-mode.el ends here
