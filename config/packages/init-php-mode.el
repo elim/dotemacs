d00d4d91948ef2bddc5cb9f0a22e2031f2fac1d0
Takeru Naito <takeru.naito@gmail.com>
2018-01-02 01:40:46 +0900

    Apply the use-package format to the php-mode configurations

--- config/packages/init-php-mode.el
+++ config/packages/init-php-mode.el
@@ -2,20 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(with-eval-after-load-feature 'php-mode
-  (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+(use-package php-mode
+  :bind (:map php-mode-map
+              ("C-c C-[" . beginning-of-defun)
+              ("C-c C-]" . end-of-defun))
+
+  :init
+  (set-variable 'php-mode-coding-style 'psr2)
 
   (defun elim:php-mode-hook-func ()
-    (define-key php-mode-map (kbd "C-c C-[") 'beginning-of-defun)
-    (define-key php-mode-map (kbd "C-c C-]") 'end-of-defun)
-
     (setq-local shell-file-name "/bin/sh")
-    (setq flycheck-phpcs-standard "PSR2")
-    (setq php-mode-coding-style (quote PSR-2))
+    (setq-local flycheck-phpcs-standard "PSR2")
     (php-enable-psr2-coding-style)
     (hs-minor-mode 1))
 
-  (add-hook 'php-mode-hook #'elim:php-mode-hook-func))
+  :hook (php-mode-hook . elim:php-mode-hook-func))
 
 (provide 'init-php-mode)
 
