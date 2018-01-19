22cf4bd1147aaaf48ec2373c0f3583a48da1c07c
Takeru Naito <takeru.naito@gmail.com>
2018-01-19 16:55:11 +0900

    Turn off the elim:auto-delete-trailing-whitespace when a commit log

--- config/packages/init-magit.el
+++ config/packages/init-magit.el
@@ -4,7 +4,12 @@
 
 (use-package magit
   :bind ("C-x v s" . magit-status)
+  :hook (git-commit-setup . elim:git-commit-setup-hook-func)
   :init (add-to-list 'process-coding-system-alist '("git" utf-8 . utf-8))
+  :config
+  (defun elim:git-commit-setup-hook-func ()
+    (set (make-local-variable
+          'elim:auto-delete-trailing-whitespace-enable-p) nil))
   :demand t)
 
 ;;; init-magit.el ends here
