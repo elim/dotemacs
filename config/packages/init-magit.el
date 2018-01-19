5d94913bdc8c725c15e9e085fcc03e5eddec4f71
Takeru Naito <takeru.naito@gmail.com>
2018-01-19 16:54:13 +0900

    Apply the use-package format to the magit configurations

--- config/packages/init-magit.el
+++ config/packages/init-magit.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(global-set-key (kbd "C-x v s") 'magit-status)
-(add-to-list 'process-coding-system-alist '("git" utf-8 . utf-8))
-
-(provide 'init-magit)
+(use-package magit
+  :bind ("C-x v s" . magit-status)
+  :init (add-to-list 'process-coding-system-alist '("git" utf-8 . utf-8))
+  :demand t)
 
 ;;; init-magit.el ends here
