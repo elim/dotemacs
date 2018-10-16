cb70dfc2422a3391571bdc2a65d695445f3979bf
Takeru Naito <takeru.naito@gmail.com>
2018-10-16 22:17:30 +0900

    Load the built-in version org-mode prefer the El-Get packages

--- config/packages.el
+++ config/packages.el
@@ -2,24 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;;; el-get
-;;
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(let (el-get-master-branch)
-  (unless (require 'el-get nil t)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-
-(setq el-get-user-package-directory (locate-user-emacs-file "config/packages"))
-
-;; Preferred libraries
-(el-get-bundle tarao/with-eval-after-load-feature-el)
-(el-get-bundle use-package :depends (delight))
-
-
 ;; For vue-mode.
 (el-get-bundle Fanael/edit-indirect)
 (el-get-bundle AdamNiederer/ssass-mode)
--- init.el
+++ init.el
@@ -22,12 +22,28 @@
   (when (and (file-exists-p dir) (not (member dir load-path)))
     (setq load-path (append (list dir) load-path))))
 
-;; load essential libraries.
-(load "packages")
-(load "environment")
-(load "theme")
-(load "builtins")
-(load "local" t)
+;;; el-get
+;;
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(let (el-get-master-branch)
+  (unless (require 'el-get nil t)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
+(setq el-get-user-package-directory
+      (locate-user-emacs-file "config/packages"))
+
+;; Preferred libraries
+(el-get-bundle tarao/with-eval-after-load-feature-el)
+(el-get-bundle use-package :depends (delight))
+
+(load "environment")
+(load "builtins")
+(load "packages")
+(load "theme")
+(load "local" t)
 
 ;;; init.el ends here
