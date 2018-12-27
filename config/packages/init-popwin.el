7dad128dd831db7e414b641fe8efc48c80034da2
Takeru Naito <takeru.naito@gmail.com>
2018-12-27 18:00:32 +0900

    Use the use-package macro in the popwin configurations

--- config/packages/init-popwin.el
+++ config/packages/init-popwin.el
@@ -2,14 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'popwin)
+(use-package popwin
+  :config
+  (push '("*Google Translate*") popwin:special-display-config)
+  (push '("*ginger*")           popwin:special-display-config)
+  (push '("*rephrase*")         popwin:special-display-config)
+  (popwin-mode 1)
 
-(popwin-mode 1)
-(setq popwin:popup-window-position 'bottom
-      popwin:popup-window-height 20)
-
-(push '("*Google Translate*") popwin:special-display-config)
-(push '("*ginger*")           popwin:special-display-config)
-(push '("*rephrase*")         popwin:special-display-config)
+  :custom
+  (popwin:popup-window-position 'bottom)
+  (popwin:popup-window-height 20))
 
 ;;; init-popwin.el ends here
