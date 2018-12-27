8ab8b3345c2c21500e0b947c895ce0b87a13a3d1
Takeru Naito <takeru.naito@gmail.com>
2018-12-27 18:01:20 +0900

    Use the use-package macro in the migemo configurations

--- config/packages/init-migemo.el
+++ config/packages/init-migemo.el
@@ -2,13 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'migemo)
+(use-package migemo
+  :custom
+  (migemo-coding-system 'utf-8-unix)
+  (migemo-command "/usr/local/bin/cmigemo")
+  (migemo-options '("-q" "--emacs"))
+  (migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
 
-(setq migemo-coding-system 'utf-8-unix
-      migemo-command "/usr/local/bin/cmigemo"
-      migemo-options '("-q" "--emacs")
-      migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-
-(migemo-init)
+  :config
+  (migemo-init))
 
 ;;; init-migemo.el ends here
