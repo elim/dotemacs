f3755167650230c6f03c112d1e264be2c17f5675
Takeru Naito <takeru.naito@gmail.com>
2018-12-28 12:18:12 +0900

    Use the use-package macro in the elscreen configurations

--- config/packages/init-elscreen.el
+++ config/packages/init-elscreen.el
@@ -2,41 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'elscreen)
+(use-package elscreen
+  :config
+  (elscreen-set-prefix-key [(control z)])
+  (elscreen-start)
 
-(setq elscreen-tab-display-control nil
-      elscreen-tab-display-kill-screen nil
-      elscreen-display-tab t)
+  :custom
+  (elscreen-tab-display-control nil)
+  (elscreen-tab-display-kill-screen nil)
+  (elscreen-display-tab t)
 
-(elscreen-set-prefix-key [(control z)])
-(set-face-attribute 'elscreen-tab-background-face nil
-                    :foreground "#112"
-                    :background "#ccc"
-                    :underline nil
-                    :height 1.2
-                    :box nil)
+  :custom-face
+  (elscreen-tab-background-face     ((nil (:foreground "#112" :background "#ccc" :underline nil :height 1.2 :box nil))))
+  (elscreen-tab-control-face        ((nil (:foreground "#ccc" :background "#112" :underline nil :height 1.2 :box nil))))
+  (elscreen-tab-current-screen-face ((nil (:foreground "#ccc" :background "#336" :underline nil :height 1.2 :box nil))))
+  (elscreen-tab-other-screen-face   ((nil (:foreground "#ccc" :background "#112" :underline nil :height 1.2 :box nil)))))
 
-(set-face-attribute 'elscreen-tab-control-face nil
-                    :foreground "#ccc"
-                    :background "#112"
-                    :underline nil
-                    :height 1.2
-                    :box nil)
-
-(set-face-attribute 'elscreen-tab-current-screen-face nil
-                    :foreground "#ccc"
-                    :background "#336"
-                    :underline nil
-                    :height 1.2
-                    :box nil)
-
-(set-face-attribute 'elscreen-tab-other-screen-face nil
-                    :foreground "#ccc"
-                    :background "#112"
-                    :underline nil
-                    :height 1.2
-                    :box nil)
-
-(elscreen-start)
 
 ;;; init-elscreen.el ends here
