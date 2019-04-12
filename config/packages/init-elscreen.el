97acd2e1ef62ade015f90b0b61781f6ba19832ad
Takeru Naito <takeru.naito@gmail.com>
2019-04-12 13:52:04 +0900

    :lipstick: Align the ElScreen's tab height as another line's

--- config/packages/init-elscreen.el
+++ config/packages/init-elscreen.el
@@ -13,10 +13,10 @@
   (elscreen-display-tab t)
 
   :custom-face
-  (elscreen-tab-background-face     ((nil (:foreground "#112" :background "#ccc" :underline nil :height 1.2 :box nil))))
-  (elscreen-tab-control-face        ((nil (:foreground "#ccc" :background "#112" :underline nil :height 1.2 :box nil))))
-  (elscreen-tab-current-screen-face ((nil (:foreground "#ccc" :background "#336" :underline nil :height 1.2 :box nil))))
-  (elscreen-tab-other-screen-face   ((nil (:foreground "#ccc" :background "#112" :underline nil :height 1.2 :box nil)))))
+  (elscreen-tab-background-face     ((nil (:foreground "#112" :background "#ccc" :underline nil :box nil))))
+  (elscreen-tab-control-face        ((nil (:foreground "#ccc" :background "#112" :underline nil :box nil))))
+  (elscreen-tab-current-screen-face ((nil (:foreground "#ccc" :background "#336" :underline nil :box nil))))
+  (elscreen-tab-other-screen-face   ((nil (:foreground "#ccc" :background "#112" :underline nil :box nil)))))
 
 
 ;;; init-elscreen.el ends here
