a2031713efc4c1c7af5f120e6a26c9c14116fc7e
Takeru Naito <takeru.naito@gmail.com>
2019-07-01 23:40:18 +0900

    Get rid the docker-tramp

--- config/packages/init-docker-tramp.el
+++
@@ -1,8 +0,0 @@
-;;; init-docker-tramp.el --- A setting of the docker-tramp.
-;;; Commentary:
-;;; Code:
-
-(use-package docker-tramp
-  :custom (docker-tramp-use-names t))
-
-;;; init-docker-tramp.el ends here
