acf04bb9005322e39ce9befd0b602414e443b2c0
Takeru Naito <takeru.naito@gmail.com>
2019-10-16 18:01:16 +0900

    Follow upstream updates of "install leaf"

--- init.el
+++ init.el
@@ -8,36 +8,27 @@
 
 ;;; leaf.el
 ;;
-(prog1 "prepare leaf"
-  (prog1 "package"
+(prog1 "leaf"
+  (prog1 "install leaf"
     (custom-set-variables
-     '(package-archives '(("melpa" . "https://melpa.org/packages/")
+     '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                          ("melpa" . "https://melpa.org/packages/")
                           ("gnu"   . "https://elpa.gnu.org/packages/"))))
-    (package-initialize))
-
-  (prog1 "leaf"
+    (package-initialize)
     (unless (package-installed-p 'leaf)
-      (unless (assoc 'leaf package-archive-contents)
-        (package-refresh-contents))
-      (condition-case err
-          (package-install 'leaf)
-        (error
-         (package-refresh-contents)       ; renew local melpa cache if fail
-         (package-install 'leaf))))
+      (package-refresh-contents)
+      (package-install 'leaf)))
 
-    (leaf leaf-keywords
-      :ensure t
-      :config (leaf-keywords-init)))
-
-  (prog1 "optional packages for leaf-keywords"
+  (leaf leaf-keywords
+    :ensure t
+    :config
     ;; optional packages if you want to use :hydra, :el-get,,,
     (leaf hydra :ensure t)
-    (leaf diminish :ensure t))
+    (leaf el-get :ensure t
+      :custom ((el-get-git-shallow-clone . t)))
 
-  (prog1 "el-get settings"
-    (leaf el-get
-      :ensure t
-      :custom (el-get-git-shallow-clone  . t))))
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
 (leaf *first-priority
   :doc
