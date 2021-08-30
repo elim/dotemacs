9b9e661a68588e342375af6399d611ed4e4e1aab
Takeru Naito <takeru.naito@gmail.com>
2021-08-30 13:34:52 +0900

    Set fullscreen by early-init.el

--- early-init.el
+++ early-init.el
@@ -9,5 +9,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 
+(push '(fullscreen . maximized) default-frame-alist)
+
 (provide 'early-init)
 ;;; early-init.el ends here
